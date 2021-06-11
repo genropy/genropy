5ebb6afdff201c07b397d77817075758d96d3c56
Davide Paci <davide.paci@softwell.it>
2021-06-11 17:00:49 +0200

    Small fix to glbl data, dbSelect and remoteSelect tests

--- projects/gnrcore/packages/test/menu.py
+++ projects/gnrcore/packages/test/menu.py
@@ -1,12 +1,12 @@
 # encoding: utf-8
 def config(root,application=None):
-    test = root.branch(u"!!Tests", tags="_DEV_",)
+    test = root.branch(u"!!Tests", tags="_DEV_")
     test.branch("Components", pkg="test", dir='components')
     test.branch("Controllers", pkg="test", dir='controllers')
+    test.branch("Dojo", pkg="test", dir='dojo')
     test.branch("Drag & Drop", pkg="test", dir='drag_drop')
     test.branch("HTML", pkg="test", dir='html')
+    test.branch("Input fields", pkg="test", dir='inputfields')
+    test.branch("Layout", pkg="test", dir='layout')
+    test.branch("Path", pkg="test", dir='path')
     test.branch("Services", pkg="test", dir='services')
-#    test.branch("Dojo" , pkg="test", dir='dojo')
-#    test.branch("Gnrwdg", pkg="test", dir='gnrwdg')
-#    test.branch("Dev tools", pkg="test", dir='devtools')
-#    test.branch("Tools", pkg="test", dir='tools')
