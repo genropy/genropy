c30692c09b01fa63871a1842169b12519229cac7
Saverio Porcari <>
2021-10-26 13:18:10 +0200

    importazione xlsx

--- gnrpy/gnr/web/gnrwebpage_proxy/utils.py
+++ gnrpy/gnr/web/gnrwebpage_proxy/utils.py
@@ -23,6 +23,8 @@ from gnr.core.gnrlist import getReader
 from gnr.core.gnrstring import slugify
 from gnr.core.gnrlang import gnrImport, objectExtract
 from gnr.core.gnrclasses import GnrClassCatalog
+from gnr.core.gnrlang import GnrException
+
 
 EXPORT_PDF_TEMPLATE = """
 <html lang="en">
@@ -237,8 +239,7 @@ class GnrWebUtils(GnrBaseProxy):
         try:
             reader = self.getReader(file_path,filetype=filetype)
         except Exception as e:
-            self.page.clientPublish('floating_message',message='Reader error %s' %str(e),messageType='error')
-
+            raise GnrException('Reader error %s' %str(e),messageType='error')
         
         importerStructure = importerStructure or dict()
         mainsheet = importerStructure.get('mainsheet')
