48d44b4bcf8986f9a198261ac7ca782f6cc64d5c
Francesco Porcari <francesco.porcari@softwell.it>
2021-12-04 10:28:39 +0100

    externalcal log

--- resources/common/gnrcomponents/externalcall.py
+++ resources/common/gnrcomponents/externalcall.py
@@ -94,6 +94,7 @@ class NetBagRpc(BaseComponent):
 
     skip_connection = True
     def logCall(self,methodname,result=None,parameters=None,error=None,**kwargs):
+
         with self.db.tempEnv(connectionName='system',storename=self.db.rootstore):
             tblobj = self.db.table('sys.externalcall_log')
             newrec = tblobj.newrecord(methodname=methodname,result=result,parameters=parameters,error=error)
@@ -104,17 +105,23 @@ class NetBagRpc(BaseComponent):
         self.response.content_type = 'application/xml'
         methodname = args[0]
         parameters=Bag(kwargs)
+        _debugmode = kwargs.pop('_debugmode',None)
+        loglevel = 0
         if 'pagetemplate' in kwargs:
             kwargs.pop('pagetemplate')
         if args:
             try:
                 method = self.getPublicMethod('rpc','netbag_%s' %methodname)
+                if method:
+                    loglevel =  getattr(method,'loglevel',0)
             except (GnrUserNotAllowed, GnrBasicAuthenticationError) as err:
-                self.logCall(methodname,parameters=parameters,error=str(err))
+                if _debugmode or loglevel>0:
+                    self.logCall(methodname,parameters=parameters,error=str(err))
                 return Bag(dict(error=str(err))).toXml()
             if not method:
                 error =  self.rpc_error(*args, **kwargs)
-                self.logCall(methodname,parameters=parameters,error=error)
+                if _debugmode:
+                    self.logCall(methodname,parameters=parameters,error=error)
                 return error
             args = list(args)
             args.pop(0)
@@ -125,11 +132,13 @@ class NetBagRpc(BaseComponent):
         except Exception as e:
             #import traceback
             #result = Bag(dict(error=traceback.format_exc().encode('utf8')))
-            self.logCall(methodname,parameters=parameters,error=str(e))
+            if _debugmode or loglevel>0:
+                self.logCall(methodname,parameters=parameters,error=str(e))
             return Bag(dict(error=TraceBackResolver()))
         if not isinstance(result,Bag):
             result = Bag(dict(result=result))
-        self.logCall(methodname,parameters=parameters,result=result)
+        if _debugmode or loglevel>0:
+            self.logCall(methodname,parameters=parameters,result=result)
         return result.toXml(unresolved=True)
 
     def validIpList(self):
