77e4e11b4b96a9c58a88bff2845ca88cae96d40d
Michele Bertoldi <michele.bertoldi@softwell.it>
2021-12-09 14:59:15 +0100

    fix

--- gnrpy/gnr/lib/services/rms.py
+++ gnrpy/gnr/lib/services/rms.py
@@ -109,7 +109,7 @@ class RMS(object):
             return
         p = PathResolver()
         siteconfig = p.get_siteconfig(name)
-        site_rms = dict(siteconfig.getAttr('rms')) or dict()
+        site_rms = dict(siteconfig.getAttr('rms')) if siteconfig.getAttr('rms') else dict()
         customer_code = customer_code or site_rms.get('customer_code')
         domain = domain or  site_rms.get('domain')
         if not domain:
