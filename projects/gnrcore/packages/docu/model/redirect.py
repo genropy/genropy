12a960e78b8527f9744be03c6c06f195def013c3
Davide Paci <davide.paci@softwell.it>
2022-01-13 12:24:16 +0100

    Docu: redirect logic moved from batch to table

--- projects/gnrcore/packages/docu/model/redirect.py
+++ projects/gnrcore/packages/docu/model/redirect.py
@@ -1,5 +1,8 @@
 # encoding: utf-8
 
+import os
+import errno
+
 class Table(object):
     def config_db(self,pkg):
         tbl=pkg.table('redirect', pkey='id', name_long='Redirect', name_plural='Redirects',caption_field='page_name')
@@ -18,3 +21,31 @@ class Table(object):
         tbl.aliasColumn('page_name', '@page_id.name', name_long='!![en]Page name')
         tbl.aliasColumn('old_handbook_path', '@old_handbook_id.sphinx_path', name_long='!![en]Handbook path')
         tbl.aliasColumn('old_handbook_url', '@old_handbook_id.handbook_url', name_long='!![en]Handbook url')
+
+    def makeRedirect(self, redirect_rec):
+        old_url = redirect_rec['old_url']
+        old_page_to_red = old_url.replace(redirect_rec['old_handbook_url'],'')
+        old_page_path = '/sphinx/build' + old_page_to_red
+        sn = self.db.application.site.storageNode(redirect_rec['old_handbook_path']+old_page_path)
+        new_url = redirect_rec['new_url']
+        html_text = self.defaultHtmlRedirect(new_url)
+
+        #Check if folder exists, otherwise creates it
+        if not os.path.exists(os.path.dirname(sn.internal_path)):
+            try:
+                os.makedirs(os.path.dirname(sn.internal_path))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+        with open(sn.internal_path,"w") as html_file:
+            html_file.write(html_text)
+
+    def defaultHtmlRedirect(self, new_url):
+        return """<html>
+                    <head>
+                        <meta http-equiv="refresh" content="1; url={new_url}" />
+                        <script>
+                          window.location.href = "{new_url}"
+                        </script>
+                    </head>
+                </html>""".format(new_url=new_url)
