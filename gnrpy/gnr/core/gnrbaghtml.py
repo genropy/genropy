d14d39dbd599b3bbe4f344a928621bcb81601ae0
Francesco Porcari <francesco.porcari@softwell.it>
2022-03-11 10:23:15 +0100

    adaptParentSize in multipage letterhead print

--- gnrpy/gnr/core/gnrbaghtml.py
+++ gnrpy/gnr/core/gnrbaghtml.py
@@ -208,15 +208,13 @@ class BagToHtml(object):
             self.pages_folder = os.path.splitext(self.filepath)[0]
             self.splittedPages_data = []
         self.showTemplate(hideTemplate is not True)
-        self.htmlTemplate = None
-        self.prepareTemplates()
         self.page_debug = page_debug or self.page_debug
         self.newBuilder()
         result = self.createHtml(filepath=self.filepath,body_attributes=self.body_attributes)
         return result
 
     def newBuilder(self):
-        
+        self.prepareTemplates()
         self.builder = GnrHtmlBuilder(page_width=self.page_width, page_height=self.page_height,
                                     page_margin_top=self.page_margin_top, page_margin_bottom=self.page_margin_bottom,
                                     page_margin_left=self.page_margin_left, page_margin_right=self.page_margin_right,
@@ -247,11 +245,12 @@ class BagToHtml(object):
     def prepareTemplates(self):
         """Set the correct value of every measure of the page: height, width, header, footer, margins"""
         top_layer = Bag()
-        if not self.htmlTemplate:
-            self.htmlTemplate = self.templateLoader(letterhead_id=self.letterhead_id,name=self.templates)
-            if self.htmlTemplate:
-                self.htmlTemplate.walk(self.fillLetterheadSourceData)
-                top_layer =  self.htmlTemplate['#%i' %(len(self.htmlTemplate)-1)]
+        self.htmlTemplate = self.templateLoader(letterhead_id=self.letterhead_id,name=self.templates)
+        if self.htmlTemplate:
+            baseTemplate = Bag(self.htmlTemplate)
+            baseTemplate.pop('next_letterhead')
+            baseTemplate.walk(self.fillLetterheadSourceData)
+            top_layer =  baseTemplate['#%i' %(len(baseTemplate)-1)]
         d = self.__dict__
         paper_height = float(d.get('page_height') or top_layer['main.page.height'] or self.paperHeight)
         paper_width = float(d.get('page_width') or top_layer['main.page.width'] or self.paperWidth)
