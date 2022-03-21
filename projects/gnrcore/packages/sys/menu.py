54c3524200908a2c152ecc4b524dddbb14feb146
Davide Paci <davide.paci@softwell.it>
2022-03-21 11:52:07 +0100

    Requirements and new menus

--- projects/gnrcore/packages/sys/menu.py
+++ projects/gnrcore/packages/sys/menu.py
@@ -1,19 +1,18 @@
 # encoding: utf-8
-def config(root,application=None):
-    system = root.branch(u"!!System", tags="sysadmin,_DEV_,superadmin")
-    system.webpage("Onering", filepath="/sys/onering")
-    system.thpage("Errors", table='sys.error')
-    system.thpage("Batch log", table='sys.batch_log')
-    system.thpage("Upgrades", table='sys.upgrade')
-    system.thpage("Services", table='sys.service')
-    system.thpage("Tasks", table='sys.task',tags='_DEV_')
-    system.webpage("Db Structure", filepath="/sys/dbstruct")
-    system.webpage("Startup data manager", filepath="/sys/startupdata_manager",tags='_DEV_')
-    system.webpage("Package editor", filepath="/sys/package_editor",tags='_DEV_')
-    system.webpage("Localization editor", filepath="/sys/localizationeditor",
-                    tags='_DEV_,_TRD_,superadmin')
-    system.webpage("GnrIDE", filepath="/sys/gnride",tags='_DEV_')
-    system.webpage("POD dashboard", filepath="/sys/pod_dashboard",tags='_DEV_')
-
-    system.thpage("Widgets", table='sys.widget')
+class Menu(object):
+    def config(self,root,**kwargs):
+        system = root.branch(u"!!System", tags="sysadmin,_DEV_,superadmin")
+        system.webpage(u"Onering", tags="", filepath="/sys/onering")
+        system.thpage(u"Errors", table="sys.error", tags="")
+        system.thpage(u"Batch log", table="sys.batch_log", tags="")
+        system.thpage(u"Upgrades", table="sys.upgrade", tags="")
+        system.thpage(u"Services", table="sys.service", tags="")
+        system.thpage(u"Tasks", table="sys.task", tags="_DEV_")
+        system.webpage(u"Db Structure", tags="", filepath="/sys/dbstruct")
+        system.webpage(u"Startup data manager", tags="_DEV_", filepath="/sys/startupdata_manager")
+        system.webpage(u"Package editor", tags="_DEV_", filepath="/sys/package_editor")
+        system.webpage(u"Localization editor", tags="_DEV_,_TRD_,superadmin", filepath="/sys/localizationeditor")
+        system.webpage(u"GnrIDE", tags="_DEV_", filepath="/sys/gnride")
+        system.webpage(u"POD dashboard", tags="_DEV_", filepath="/sys/pod_dashboard")
+        system.thpage(u"Widgets", table="sys.widget", tags="")
 
--- projects/gnrcore/packages/test15/menu.py
+++ projects/gnrcore/packages/test15/menu.py
@@ -1,8 +1,10 @@
 # encoding: utf-8
-def config(root,application=None):
-    test15 = root.branch(u"!!Tests", tags="_DEV_",)
-    test15.branch("Components", pkg="test15",dir='components')
-    test15.branch("Dojo" ,pkg="test15",dir='dojo')
-    test15.branch("Gnrwdg", pkg="test15",dir='gnrwdg')
-    test15.branch("Dev tools", pkg="test15",dir='devtools')
-    test15.branch("Tools", pkg="test15",dir='tools')
+class Menu(object):
+    def config(self,root,**kwargs):
+        tests = root.branch(u"!!Tests", tags="_DEV_")
+        tests.branch(u"Components", tags="", pkg="test15", dir="components")
+        tests.branch(u"Dojo", tags="", pkg="test15", dir="dojo")
+        tests.branch(u"Gnrwdg", tags="", pkg="test15", dir="gnrwdg")
+        tests.branch(u"Dev tools", tags="", pkg="test15", dir="devtools")
+        tests.branch(u"Tools", tags="", pkg="test15", dir="tools")
+
