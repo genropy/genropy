ec4a34d54c00a66c6115a7840033d4c043baa29a
Francesco Porcari <francesco.porcari@softwell.it>
2022-03-22 15:54:29 +0100

    fix

--- gnrpy/gnr/lib/services/rms.py
+++ gnrpy/gnr/lib/services/rms.py
@@ -126,9 +126,11 @@ class RMS(object):
         rmsbag = self.buildRmsService(name,domain=domain,customer_code=customer_code)
         rms_instance_attr = rmsbag.getAttr('rms')
         customer_code = rms_instance_attr.get('customer_code') or self.customer_code
-        return NetBag(self.authenticatedUrl,'register_instance',code=name,
+        result = NetBag(self.authenticatedUrl,'register_instance',code=name,
                             domain=domain,
                             pod_token=self.token,
                             instance_token= rms_instance_attr['token'],
                             customer_code=customer_code)()
+        print(result)
+        return result
 
