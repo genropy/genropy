0da900bb2fc58ab20b5bad7c42eb516ab108365a
fporcari <francesco.porcari@softwell.it>
2022-08-01 15:32:16 +0200

    fix format totalize

--- gnrjs/gnr_d11/js/genro_wdg.js
+++ gnrjs/gnr_d11/js/genro_wdg.js
@@ -2030,7 +2030,9 @@ dojo.declare("gnr.GridChangeManager", null, {
         }
         var totvalue = this.grid.storebag().sum(this.grid.datamode=='bag'?k:'#a.'+k,this.grid.cellmap[k].totalize_strict);
         if(!isNullOrBlank(totvalue)){
-            totvalue = Math.round10(totvalue);
+            let cellformat = this.grid.cellmap[k]._formats? this.grid.cellmap[k]._formats.format:undefined;        ;
+            console.log('updateTotalizer cellformat',cellformat)
+            totvalue = Math.round10(totvalue,cellformat);
         }
         this.sourceNode.setRelativeData(this.grid.cellmap[k].totalize,totvalue);
         this.sourceNode.publish('onUpdateTotalize',{'column':k,'value':totvalue});
