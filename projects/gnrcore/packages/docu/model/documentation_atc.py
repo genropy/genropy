ec004a2188022ad3366bd8917a1975d698afba5f
Davide Paci <davide.paci@softwell.it>
2022-08-31 09:52:07 +0200

    Documentation atc minor fix to touch procedure

--- projects/gnrcore/packages/docu/model/documentation_atc.py
+++ projects/gnrcore/packages/docu/model/documentation_atc.py
@@ -11,4 +11,5 @@ class Table(AttachmentTable):
     @metadata(doUpdate=True)
     def touch_updateAtcFilepath(self,record,old_record=None):
         "Update after S3 configuration"
+        record['filepath'] = record['filepath'].replace('docu_documentation', 'documentation:attachments')
         record['filepath'] = record['filepath'].replace('home:docu_documentation', 'documentation:attachments')
