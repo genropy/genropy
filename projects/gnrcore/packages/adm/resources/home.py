7a4d56cc7330403228cdf3fb082735b887a15ded
fporcari <francesco.porcari@softwell.it>
2022-10-17 13:59:50 +0200

    default homepage

--- projects/gnrcore/packages/adm/resources/home.py
+++ projects/gnrcore/packages/adm/resources/home.py
@@ -1,11 +1,21 @@
 from gnr.web.gnrbaseclasses import BaseComponent
 
-def HomePage(BaseComponent):
+class HomePage(BaseComponent):
+    py_requires = 'gnrcomponents/tpleditor'
     auth_main = 'user'
-    
+
     def main(self,root,**kwargs):
-        root.templateChunk(position='absolute',
+        tplname = f'homepage_{self.user}'
+        tpl = self.loadTemplate(f'adm.user:{tplname}',missingMessage='_')
+        if tpl=='_':
+            tplname = f"homempage_{self.rootenv['user_group_code']}"
+            tpl = self.loadTemplate(f'adm.user:{tplname}',missingMessage='_')
+        if tpl == '_':
+            tplname = 'homepage'
+        root.dataFormula('main.user_id','user_id',user_id=self.rootenv['user_id'],_onBuilt=1)
+        root.templateChunk(position='absolute',datapath='main',
                             top='0',bottom='0',
                             left='0',right='0',
                             template='homepage',
-                            table='adm.user')
+                            table='adm.user',
+                            record_id='^.user_id')
--- projects/gnrcore/packages/adm/webpages/home.py
+++ projects/gnrcore/packages/adm/webpages/home.py
@@ -9,20 +9,4 @@
 
 
 class GnrCustomWebPage(object):
-    py_requires = 'gnrcomponents/tpleditor'
-    #py_requires = 'home:HomePage'
-    def main(self,root,**kwargs):
-        tplname = f'homepage_{self.user}'
-        tpl = self.loadTemplate(f'adm.user:{tplname}',missingMessage='_')
-        if tpl=='_':
-            tplname = f"homempage_{self.rootenv['user_group_code']}"
-            tpl = self.loadTemplate(f'adm.user:{tplname}',missingMessage='_')
-        if tpl == '_':
-            tplname = 'homepage'
-        root.dataFormula('main.user_id','user_id',user_id=self.rootenv['user_id'],_onBuilt=1)
-        root.templateChunk(position='absolute',datapath='main',
-                            top='0',bottom='0',
-                            left='0',right='0',
-                            template='homepage',
-                            table='adm.user',
-                            record_id='^.user_id')
+    py_requires = 'home:HomePage'
