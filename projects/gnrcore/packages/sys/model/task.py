610159817f5446617d86f91869602264b3ac239b
fporcari <francesco.porcari@softwell.it>
2022-11-03 11:08:42 +0100

    task no parameters fix

--- projects/gnrcore/packages/sys/model/task.py
+++ projects/gnrcore/packages/sys/model/task.py
@@ -54,10 +54,12 @@ class Table(object):
                 return '*'
             else:
                 return False
+        if not task['minute']:
+            return False
         months =  [int(x.strip()) for x in task['month'].split(',')] if task['month'] else range(1,13)
         days = [int(x.strip()) for x in task['day'].split(',')] if task['day'] else range(1,32)
         hours = [int(x.strip()) for x in task['hour'].split(',')] if task['hour'] else range(0,24)
-        minutes = [int(x.strip()) for x in task['minute'].split(',')] if task['minute'] else range(0,60)
+        minutes = [int(x.strip()) for x in task['minute'].split(',')]
         hm = []
         for h in hours:
             for m in minutes:
