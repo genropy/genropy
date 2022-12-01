1f7e18fd8120953a49a4a3df667ace04fc8954b7
Davide Paci <davide.paci@softwell.it>
2022-12-01 16:42:36 +0100

    videotrack fixed

--- projects/gnrcore/packages/test/webpages/tools/videotrack.py
+++ projects/gnrcore/packages/test/webpages/tools/videotrack.py
@@ -7,42 +7,21 @@
 "Test page description"
 from builtins import object
 class GnrCustomWebPage(object):
-    py_requires="gnrcomponents/testhandler:TestHandlerFull,gnrcomponents/tpleditor:TemplateEditor"
+    py_requires="gnrcomponents/testhandler:TestHandlerBase"
     
-    def test_0_videotrack_static(self,pane):
-        """First test description"""
-        bc = pane.borderContainer(height='500px')
-        pane = bc.contentPane(overflow='hidden',region='center')
-        pane.video(src='https://www.youtube.com/watch?v=DoG5gTLv0Ik',height='100%',width='100%',
-                    border=0,controls=True,nodeId='preview_video',
-                    tracks=[dict(src='/_site/screencasts/pippo.vtt?zzz',
-                                kind='subtitles',srclang='it')])
+    def test_0_videotrack_dynamic(self,pane):
+        "Widget video shows a video from its url. You can indicate video path dinamically"
+        pane.formbuilder(width='100%', colswidth='100%').textbox('^.video_url', lbl='Video url', width='100%')
+        pane.data('.video_url', 'https://community.genropy.net/_storage/social/video/YGvmpOe7Nfea_L08ojbcoQ.mp4')
+        pane.video(src='^.video_url',
+                    height='100%',width='100%',
+                    border=0,controls=True,nodeId='preview_video')
+                    #tracks=[dict(src='/_site/screencasts/pippo.vtt?zzz',
+                    #            kind='subtitles',srclang='it')])
+                    #In this case I have no subtitles available
 
-    def test_1_videotrack_dinamic(self,pane):
-        """First test description"""
-        bc = pane.borderContainer(height='500px')
-        top = bc.contentPane(region='top')
-        fb = top.formbuilder(cols=1,border_spacing='3px')
-        fb.dbSelect(value='^.video_id',lbl='Video',selected_url='.video_url',dbtable='video.video')
-        fb.filteringSelect(value='^.language',lbl='Lang',values='it,en,fr',
-                        default_value='it',validate_onAccept="""
-                        var video_url = GET .video_url;
-                        PUT .video_url = null
-                        SET .video_url = video_url;
-                        """)
-
-        pane = bc.contentPane(overflow='hidden',region='center')
-        pane.video(src='^.video_url',height='100%',width='100%',
-                    border=0,controls=True,nodeId='preview_video',
-                    tracks=[dict(src='/video/index/vtt/$_video_id/$kind/$srclang.vtt',
-                                kind='subtitles',srclang='^.language',_video_id='^.video_id',
-                                label='Subtitle'),
-                            dict(src='/video/index/vtt/$_video_id/$kind/$srclang.vtt',
-                                kind='chapters',srclang='^.language',_video_id='^.video_id',
-                                label='Chapter')])
-
-    def test_2_videotrack_semistatic(self,pane):
-        """First test description"""
+    def test_1_videotrack_semistatic(self,pane):
+        "Widget VideoPlayer is an alternative way. It supports timerange and resizing"
         bc = pane.borderContainer(height='500px')
         top = bc.contentPane(region='top',height='50px',background='red',splitter=True)
         fb = top.formbuilder(cols=1,border_spacing='3px')
@@ -55,16 +34,17 @@ class GnrCustomWebPage(object):
         bc.contentPane(region='right',width='50px',splitter=True,background='navy')
         bc.contentPane(region='bottom',height='50px',splitter=True,background='silver')
 
-        pane = bc.VideoPlayer(url='https://www.youtube.com/watch?v=DoG5gTLv0Ik',
+        pane = bc.VideoPlayer(url='https://community.genropy.net/_storage/social/video/YGvmpOe7Nfea_L08ojbcoQ.mp4',
                     datapath='.myvideo',
                     region='center',
                     manageCue=True,
                     timerange='^.range',
                     selfsubscribe_addCue='console.log("fffff",$1);',
-                   border=0,nodeId='preview_videoplayer',
-                   subtitlePane=True,
-                   tracks=[dict(src='/video/index/vtt/KMq3Rzs6MMW3so_vWdYFXg/subtitles/it.vtt',
-                               kind='subtitles',srclang='it',label='Subtitle',
-                               cue_path='.mainsub',hidden=True)
-                   ])
+                    border=0,nodeId='preview_videoplayer',
+                    subtitlePane=True)
+                   #tracks=[dict(src='/video/index/vtt/KMq3Rzs6MMW3so_vWdYFXg/subtitles/it.vtt',
+                   #           kind='subtitles',srclang='it',label='Subtitle',
+                   #           cue_path='.mainsub',hidden=True)
+                   #])
+                   #In this case I have no subtitles available
         
