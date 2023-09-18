8db7d1986f34a3db6057823cf59460fe8918ac0b
fporcari <francesco.porcari@softwell.it>
2023-09-18 08:46:52 +0200

    f

--- resources/common/pwa/worker.js
+++ resources/common/pwa/worker.js
@@ -25,10 +25,31 @@ event.waitUntil(self.registration.showNotification(title, options));
 
 self.addEventListener('notificationclick', function(event) {
     console.log('[Service Worker] Notification click Received.');
-
+    this.clients.matchAll().then(m=>{console.log('match al result',m)});
+    let json =  event.notification.data;
+    console.log('confirmUrl',json.confirm_url)
+    
+    fetch(json.confirm_url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({'content':json}) // body data type must match "Content-Type" header
+    }).then(response=>{
+        console.log('notifica accettata')
+    });
     event.notification.close();
-    //notify to the server the notification has been clicked
+        //notify to the server the notification has been clicked
     event.waitUntil(clients.openWindow(event.notification.data.url));
+
+
+    
 });
 
 self.addEventListener('pushsubscriptionchange', function(event) {
