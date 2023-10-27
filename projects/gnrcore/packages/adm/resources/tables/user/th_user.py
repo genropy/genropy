b292965d785ff7287093a25f6b5fa59c2000f643
Davide Paci <davide.paci@softwell.it>
2023-10-27 11:11:30 +0200

    User preferences Form for desktop and mobile

--- projects/gnrcore/packages/adm/resources/tables/user/th_user.py
+++ projects/gnrcore/packages/adm/resources/tables/user/th_user.py
@@ -193,23 +193,21 @@ class FormProfile(BaseComponent):
         
     def th_form(self,form):
         bc = form.center.borderContainer()
-        fb = bc.contentPane(region='top',datapath='.record',margin_top='10px').formbuilder(cols=4, 
-                        border_spacing='4px',colswidth='auto',width='100%',fld_width='100%')
+        top = bc.borderContainer(region='top',datapath='.record',margin_top='10px', height='210px' if self.isMobile else '150px')
+        
+        fb = top.contentPane(region='center').mobileFormBuilder(cols=1 if self.isMobile else 2)
         fb.field('firstname',lbl='!!Firstname')
         fb.field('lastname',lbl='!!Lastname')
-        fb.div()
-        fb.img(src='^.photo',crop_height='100px',crop_width='100px',margin_left='25px',
-                    crop_border='2px dotted silver',crop_rounded=6,edit=True,
-                    placeholder=True,upload_folder='*',takePicture=True,rowspan=4)
         fb.field('username',lbl='!!Username',validate_nodup=True,validate_notnull_error='!!Exists',protected=True)
         fb.field('locale', lbl='!!Locale',tag='combobox',values='en_En:')
-        fb.br()
         fb.field('email', lbl='!!Email',colspan=2,width='100%')
-        #
-        #fb.field('sms_login', html_label=True)
-        #fb.field('sms_number',hidden='^.sms_login?=!#v',colspan=2,width='12em')
-        tc = bc.tabContainer(margin='2px',region='center')
-        self.adm_profile_tabs(tc)
+        
+        right = top.contentPane(region='right', width='104px', margin='10px 25px')
+        right.img(src='^.photo',crop_height='100px',crop_width='100px',
+                    crop_border='2px dotted silver',crop_rounded=6,edit=True,
+                    placeholder=True,upload_folder='*',takePicture=True,rowspan=4)
+        
+        self.adm_profile_tabs(bc.tabContainer(margin='2px',region='center'))
     
     @customizable
     def adm_profile_tabs(self,tc):
