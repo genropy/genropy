97f8249cdf36ad98fc0fb15be618c9732a0511ce
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2023-12-14 16:35:25 +0100

    added pavement tasks for deprection warning, to support the transition to the new build/install tools and strategy.

---
+++ gnrpy/pavement.py
@@ -0,0 +1,30 @@
+# Transitional paver task definition, to support
+# the migration to the new build and installation
+# tools.
+
+from paver.easy import *
+from paver.setuputils import setup, find_packages
+
+@task
+def install():
+    print("""Deprecation error: paver is no more supported!
+    
+Please refer to documention on https://www.genropy.org/
+
+'paver install' command has been replaced by:
+
+    pip install .
+""")
+
+@task
+def develop(options):
+        print("""Deprecation error: paver is no more supported!
+    
+Please refer to documention on https://www.genropy.org/
+
+'paver develop' command has been replaced by:
+
+    ./setup.py develop
+""")
+
+
