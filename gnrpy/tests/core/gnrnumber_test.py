89b773776938dd26ef69decfc768c43c60b9d6cc
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2024-01-04 15:23:36 +0100

    gnr.core.gnrnumber coverage to 100%
    Please note: it seems that some functions in the module should
    be deprecated or maybe documented, it's not clear what they're doing.
    
    Surely they're not used within gnrpy namespace, but being a public API
    they could be used in projects.

--- gnrpy/tests/core/gnrnumber_test.py
+++ gnrpy/tests/core/gnrnumber_test.py
@@ -1,4 +1,43 @@
-from gnr.core import gnrnumber
+from gnr.core import gnrnumber as gn
+import decimal
 
-def test():
-    pass
+def test_decimalRound():
+    res = gn.decimalRound(17.382)
+    assert res == decimal.Decimal('17.38')
+    
+    res = gn.decimalRound(17.382, places=3)
+    assert res == decimal.Decimal('17.382')
+    
+    res = gn.decimalRound(17.382, places=1, rounding=decimal.ROUND_UP)
+    assert res == decimal.Decimal('17.4')
+
+
+def test_floatToDecimal():
+
+    res = gn.floatToDecimal(None)
+    assert res is None
+
+    res = gn.floatToDecimal(17.352)
+    assert res == decimal.Decimal("17.352")
+
+    res = gn.floatToDecimal(17.352, places=2)
+    assert res == decimal.Decimal("17.35")
+
+
+def test_calculateMultiPerc():
+    # FIXME: this function is not used, apparently
+    # and I'm wondering what it could be used for - CRG
+    res = gn.calculateMultiPerc(None)
+    assert res is None
+    res = gn.calculateMultiPerc("10+100")
+    assert res == decimal.Decimal('100.00')
+
+
+def test_partitionTotals():
+    # FIXME: same as above. maybe it's black magic?
+    res = list(gn.partitionTotals([1,2,3], [10,70,20]))
+    assert res[0][0] == decimal.Decimal('0.10')
+
+    res = list(gn.partitionTotals("1234", [10,70,20]))
+    assert res[0][0] == decimal.Decimal('123.4')
+    
