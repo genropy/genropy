71eda7c59feec53fbec43c37b14f3bfafe27cd23
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2024-01-04 16:10:44 +0100

    skipping tests for gnrgit, due to its nature.
    added support for git remotes using also git:// protocol, since
    it supported only https schemas by default

--- gnrpy/tests/core/gnrgit_test.py
+++ gnrpy/tests/core/gnrgit_test.py
@@ -1,4 +1,6 @@
-from gnr.core import gnrgit
+from gnr.core import gnrgit as gg
 
-def test():
+def test_GnrGit():
+    # we only test the import of the module,
+    # please see gnr.core.gnrgit for details
     pass
