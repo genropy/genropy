4d721ee4350edc1f9187c04bb454ca1aab982816
Michele Bertoldi <michele.bertoldi@softwell.it>
2024-02-01 10:18:23 +0100

    imgtopdf

---
+++ gnrpy/gnr/lib/services/imgtopdf.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env pythonw
+# -*- coding: utf-8 -*-
+
+import os
+import tempfile
+
+from gnr.core.gnrdecorator import extract_kwargs
+
+from gnr.core.gnrlang import  GnrException
+
+
+from gnr.lib.services import GnrBaseService,BaseServiceType
+from gnr.lib.services.storage import StorageNode
+
+
+class ImgToPdfError(GnrException):
+    pass
+    
+
+class ServiceType(BaseServiceType):
+    def conf_imgtopdf(self):
+        return dict(implementation='imgtopdf')
+
+
+class ImgToPdfService(GnrBaseService):
+    def __init__(self,parent,**kwargs):
+        self.parent = parent
+
+    def printBodyStyle(self):
+        return "font-size:12px;font-family: Arial, Verdana, sans-serif;margin-top:0;margin-bottom:0;margin-left:0;margin-right:0;-webkit-text-size-adjust:auto;"
+
+    def standardPageHtmlTemplate(self,bodyStyle=None):
+        bodyStyle = bodyStyle or self.printBodyStyle()
+        head ="""<head> 
+                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+                    <style> 
+                        .gnrlayout{position:absolute;} 
+                        body{%s}
+                        .letterhead_page{page-break-before:always;} 
+                        .letterhead_page:first-child{page-break-before:avoid;}
+                    </style>
+                </head>
+                     """%bodyStyle
+        body = "<body>%s</body>"
+        return """<html> 
+                    %s 
+                    %s
+                 </html>""" %(head,body)
+
+    def createTempHtmlFile(self,htmlText,htmlTemplate=None,bodyStyle=None):
+        if not '<html' in htmlText:
+            htmlTemplate = htmlTemplate or self.standardPageHtmlTemplate(bodyStyle)
+            htmlText = htmlTemplate %htmlText
+        tmp = tempfile.NamedTemporaryFile(prefix='temp', suffix='.html',delete=False)
+        tmp.write(htmlText.encode())
+        url = tmp.name
+        tmp.close()
+        return url
+    
+
+    @extract_kwargs(pdf=True)
+    def imgToPdf(self, srcPath, destPath,**kwargs): #srcPathList per ridurre i processi?
+
+        srcNode = self.parent.storageNode(srcPath)
+        destNode = self.parent.storageNode(destPath)
+        return self.writePdf(srcNode, destNode,**kwargs)
+
+    def writePdf(self,srcNode, destNode, **kwargs):
+        #override
+        pass
+
---
+++ resources/common/services/imgtopdf/imgtopdf.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from gnr.lib.services.imgtopdf import ImgToPdfService
+
+class Service(ImgToPdfService):
+    def writePdf(self,srcNode, destNode, **kwargs):
+        from PIL import Image
+        with srcNode.open('rb') as infile:
+            with destNode.open('wb') as outfile:
+                Image.open(infile).save(outfile, "PDF" ,resolution=100.0, save_all=True)
