7173061e5980081b3de7a5e3aab46e2196b30064
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2024-02-08 16:03:18 +0100

    wsgiserve cmdline parse use GnrCliArgParse for consistency

--- gnrpy/gnr/web/serverwsgi_legacy.py
+++ gnrpy/gnr/web/serverwsgi_legacy.py
@@ -7,8 +7,8 @@ import sys
 import re
 import atexit
 import logging
-import argparse
 
+from gnr.core.cli import GnrCliArgParse
 from gnr.core.gnrbag import Bag
 from gnr.core.gnrdict import dictExtract
 from gnr.web.gnrwsgisite import GnrWsgiSite
@@ -161,7 +161,7 @@ class Server(object):
                       'error': logging.ERROR,
                       'critical': logging.CRITICAL}
 
-    parser = argparse.ArgumentParser(description=summary)
+    parser = GnrCliArgParse(description=summary)
     parser.add_argument('-L', '--log-level', dest="log_level", metavar="LOG_LEVEL",
                       help="Logging level",
                       choices=list(LOGGING_LEVELS.keys()),
