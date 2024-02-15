5f432f855d4fc0eff32c2f3bddc65c38dfaf5682
Davide Paci <davide.paci@softwell.it>
2024-02-15 17:03:10 +0100

    Base view user tags

--- projects/gnrcore/packages/adm/resources/tables/user_tag/th_user_tag.py
+++ projects/gnrcore/packages/adm/resources/tables/user_tag/th_user_tag.py
@@ -6,6 +6,20 @@
 
 from gnr.web.gnrbaseclasses import BaseComponent
 
+
+class View(BaseComponent):
+    def th_struct(self,struct):
+        r = struct.view().rows()
+        r.fieldcell('tag_id', width='20em')
+        r.fieldcell('@tag_id.note', width='100%')
+        
+    def th_order(self):
+        return 'tag_id'
+        
+    def th_query(self):
+        return dict(column='tag_id',op='contains', val='')
+       
+
 class ViewFromUser(BaseComponent):
     def th_struct(self,struct):
         r = struct.view().rows()
@@ -29,6 +43,7 @@ class ViewFromGroup(BaseComponent):
     def th_order(self):
         return 'tag_id'
 
+
 class ViewFromTag(BaseComponent):
     def th_hiddencolumns(self):
         return '$user_id,$user,$group_code'
@@ -43,3 +58,14 @@ class ViewFromTag(BaseComponent):
         
    #def th_query(self):
    #    return dict(column='user',op='contains', val='')
+    
+class Form(BaseComponent):
+    
+    def th_form(self, form):
+        pane = form.record
+        fb = pane.formbuilder(cols=2, border_spacing='4px')
+        fb.field('tag_id')
+        fb.field('user_id')
+
+    def th_options(self):
+        return dict(dialog_height='400px', dialog_width='600px')
