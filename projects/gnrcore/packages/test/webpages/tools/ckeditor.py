828a9e49fb30e0983b3a973fdb38a820a63e3413
Davide Paci <davide.paci@softwell.it>
2024-02-27 16:23:23 +0100

    A few ckeditor tests

--- projects/gnrcore/packages/test/webpages/tools/ckeditor.py
+++ projects/gnrcore/packages/test/webpages/tools/ckeditor.py
@@ -8,23 +8,26 @@ class GnrCustomWebPage(object):
         fb = pane.formbuilder(cols=2,border_spacing='3px')
         fb.textbox(value='^.height',lbl='Height')
         fb.textbox(value='^.width',lbl='Width')
-        pane.ckeditor(value='^.testdata',constrain_height='^.height',
+        pane.ckeditor(value='^.testdata', constrain_height='^.height',
                         config_disableNativeSpellChecker=False,
                         config_stylesSet='/_site/styles/style_pippo.js',
-                        constrain_width='^.width',constrain_border='1px solid red')
+                        constrain_width='^.width', constrain_border='1px solid red')
 
-    def test_2_toolbar(self, pane):
-        pane.ckEditor(value='^.testdata', toolbar=False)    #config_toolbarCanCollapse=True)
+    def test_2_toolbar(self, pane):                             #DP Da sistemare
+        "Toolbar can be full or can be hidden"
+        pane.ckEditor(value='^.testdata', toolbar=False, toolbarCanCollapse=True)    #config_toolbarCanCollapse=True)
                                                             #toolbar values?
 
     def test_3_gallery_handler(self,pane):
+        "Image palette to drag and drop images into editor"
         frame = pane.framePane(height='300px')
         bar = frame.top.slotToolbar('*,xxx,5')
         bar.dataFormula('^testpath','p',_onStart=True,p='')
         bar.xxx.imgPickerPalette(folders='rsrc:common/html_pages/images:Image HTML,rsrc:common/icons:Icons',dockButton=True)
         frame.center.contentPane(overflow='hidden').ckEditor(value='^.testgallery')
 
-    def test_4_simpleTextAreaEditor(self,pane):
+    def test_4_simpleTextAreaEditor(self,pane):                 #DP Da sistemare
+        "Quick editor version"
         fb = pane.formbuilder(cols=1,border_spacing='3px')
         fb.quickEditor(value='^.test',nodeId='aaa',
                         height='100px',
@@ -34,7 +37,8 @@ class GnrCustomWebPage(object):
         fb.textbox(value='^.aaa',lbl='field 2')
         fb.textbox(value='^.ooo',lbl='field 3')
 
-    def test_5_simpleTextAreaEditor(self,pane):
+    def test_5_simpleTextAreaEditor(self,pane):                 #DP Da sistemare, qui proviamo la floating toolbar
+        "Floating toolbar"
         pane.div(_class='quickEditor',height='100px',width='400px').ckEditor(value='^.ccc',
                     constrain_margin_top='1px',
                     constrain_margin='2px',
@@ -42,7 +46,6 @@ class GnrCustomWebPage(object):
 
     def test_6_simpleTextAreaInGrid(self,pane):
         grid = pane.contentPane(region='center').quickGrid(value='^.griddata',
-
                         height='500px',width='700px' ,border='1px solid silver',
                         default_description='<span style="color:red">ciao</span> come <i>va?</i>'
                         #connect_onCellDblClick='console.log("sss")'
