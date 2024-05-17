b6138b229bebb6b6c9c7d2d3be56267db4dd1e71
Davide Paci <davide.paci@softwell.it>
2024-05-17 17:18:49 +0200

    Content history

---
+++ projects/gnrcore/packages/docu/model/content_history.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Table(object):
+    def config_db(self, pkg):
+        tbl = pkg.table('content_history', pkey='id', name_long='!!Content history', 
+                        name_plural='!!Contents history', caption_field='version',
+                        order_by='$__ins_ts DESC')
+        self.sysFields(tbl)
+
+        tbl.column('content_id',size='22', group='_', name_long='!!Content'
+                    ).relation('content.id', relation_name='versions', mode='foreignkey', onDelete='cascade')
+        tbl.column('version', dtype='I', name_long='!!Version', name_short='!!Vers.',
+                    indexed=True, validate_notnull=True)
+        tbl.column('text', name_long='!!Text')
+
+    
+    def makeNewVersionFromContent(self, content_record):
+        last_content_version = self.readColumns(where='$content_id=:c_id', 
+                                    c_id=content_record['id'], columns='$version', limit=1) or 0
+        new_version = self.newrecord(text=content_record['text'], version=last_content_version+1, 
+                                     content_id=content_record['id'])
+        self.insert(new_version)
