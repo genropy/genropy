aeea3db2ec0656f4b55b7fae9336e791e93833b1
fporcari <francesco.porcari@softwell.it>
2024-05-27 18:43:41 +0200

    on click url

--- gnrjs/gnr_d11/js/genro_cordova.js
+++ gnrjs/gnr_d11/js/genro_cordova.js
@@ -70,6 +70,24 @@ dojo.declare("gnr.GnrCordovaHandler", null, {
 
 
             genro.notification_obj.on('notification', (data) => {
+                let on_click_url = data.additionalData.on_click_url;
+                if(on_click_url){
+                    fetch(on_click_url, {
+                        method: "POST", // *GET, POST, PUT, DELETE, etc.
+                        mode: "cors", // no-cors, *cors, same-origin
+                        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                        credentials: "same-origin", // include, *same-origin, omit
+                        headers: {
+                          //"Content-Type": "application/json",
+                           'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                        redirect: "follow", // manual, *follow, error
+                        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                        body:{message_id:data.additionalData.message_id,page_id:genro.page_id}// body data type must match "Content-Type" header
+                    }).then(response=>{
+                        console.log('clicked ts set')
+                    });
+                }
                 let url = data.additionalData.url;
                 if(url && genro.framedIndexManager){
                     let parsedUrl = parseURL(url);
