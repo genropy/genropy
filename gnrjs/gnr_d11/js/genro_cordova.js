ce4a5fa0cb1f0acb27d84f4e0679fa934f80aa35
fporcari <francesco.porcari@softwell.it>
2024-05-28 09:15:31 +0200

    fix onclick url fetch

--- gnrjs/gnr_d11/js/genro_cordova.js
+++ gnrjs/gnr_d11/js/genro_cordova.js
@@ -76,21 +76,28 @@ dojo.declare("gnr.GnrCordovaHandler", null, {
             genro.notification_obj.on('notification', (data) => {
                 let on_click_url = data.additionalData.on_click_url;
                 if(on_click_url){
+                    let bodyData = {message_id: data.additionalData.message_id, page_id: genro.page_id};
+                    let formBody = [];
+                    for (let property in bodyData) {
+                        let encodedKey = encodeURIComponent(property);
+                        let encodedValue = encodeURIComponent(bodyData[property]);
+                        formBody.push(encodedKey + "=" + encodedValue);
+                    }
+                    formBody = formBody.join("&");
                     fetch(on_click_url, {
-                        method: "POST", // *GET, POST, PUT, DELETE, etc.
-                        mode: "cors", // no-cors, *cors, same-origin
-                        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                        credentials: "same-origin", // include, *same-origin, omit
+                        method: "POST",
+                        mode: "cors",
+                        cache: "no-cache",
+                        credentials: "same-origin",
                         headers: {
-                          //"Content-Type": "application/json",
-                           'Content-Type': 'application/x-www-form-urlencoded',
+                            'Content-Type': 'application/x-www-form-urlencoded',
                         },
-                        redirect: "follow", // manual, *follow, error
-                        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                        body:{message_id:data.additionalData.message_id,page_id:genro.page_id}// body data type must match "Content-Type" header
-                    }).then(response=>{
-                        console.log('clicked ts set')
-                    });
+                        redirect: "follow",
+                        referrerPolicy: "no-referrer",
+                        body: formBody
+                        }).then(response => {
+                            console.log('clicked ts set')
+                        });
                 }
                 let url = data.additionalData.url;
                 if(url && genro.framedIndexManager){
