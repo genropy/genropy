c7f9c13ec567757d8faf822cba443a0ab679a36b
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2025-02-05 11:40:51 +0100

    removed httplib2 dependency, since we already have 'requests' as a dependency which is more supported.
    Refactored the single http call using httplib2 with requests' equivalent.

--- gnrpy/gnr/web/gnrwsgisite.py
+++ gnrpy/gnr/web/gnrwsgisite.py
@@ -3,7 +3,6 @@ import re
 import io
 import subprocess
 import urllib.request, urllib.parse, urllib.error
-import httplib2
 import _thread
 import mimetypes
 import functools
@@ -12,8 +11,11 @@ from collections import defaultdict
 from threading import RLock
 import warnings
 
+import requests
 from werkzeug.wrappers import Request, Response
-from webob.exc import WSGIHTTPException, HTTPInternalServerError, HTTPNotFound, HTTPForbidden, HTTPPreconditionFailed, HTTPClientError, HTTPMovedPermanently,HTTPTemporaryRedirect
+from webob.exc import (WSGIHTTPException, HTTPInternalServerError,
+                       HTTPNotFound, HTTPForbidden, HTTPPreconditionFailed,
+                       HTTPClientError, HTTPMovedPermanently,HTTPTemporaryRedirect )
 
 from gnr.core.gnrbag import Bag
 from gnr.core import gnrstring
@@ -698,10 +700,9 @@ class GnrWsgiSite(object):
             kwargs[k] = self.gnrapp.catalog.asTypedText(kwargs[k])
         urlargs = [url,method]+list(args)
         url = '/'.join(urlargs)
-        http = httplib2.Http()
         headers = {'Content-type': 'application/x-www-form-urlencoded'}
-        response,content = http.request(url, 'POST', headers=headers, body=urllib.parse.urlencode(kwargs))
-        return self.gnrapp.catalog.fromTypedText(content)
+        response = requests.post(url, headers=headers, data=kwargs)
+        return self.gnrapp.catalog.fromTypedText(response.text)
 
     def writeException(self, exception=None, traceback=None):
         try:
