079a06acdde297f90e3853f9a23698fcbf10eebc
Michele Bertoldi <michele.bertoldi@softwell.it>
2025-02-19 16:33:22 +0100

    added ECS and Secrets Manager methods to AWS Manager

--- projects/gnrcore/packages/sys/resources/services/cloudmanager/awsmanager.py
+++ projects/gnrcore/packages/sys/resources/services/cloudmanager/awsmanager.py
@@ -59,6 +59,43 @@ class Service(CloudManager):
     def get_hosted_zones(self):
         return self.aws_manager.Route53.get_hosted_zones()
 
+    # ECS Methods
+    def list_ecs_clusters(self):
+        return self.aws_manager.ECS.list_clusters()
+
+    def list_ecs_services(self, cluster):
+        return self.aws_manager.ECS.list_services(cluster=cluster)
+
+    def list_ecs_tasks(self, cluster, service=None):
+        return self.aws_manager.ECS.list_tasks(cluster=cluster, service=service)
+
+    def force_update_ecs_service(self, cluster, service):
+        return self.aws_manager.ECS.force_update_service(cluster=cluster, service=service)
+
+    # Secrets Manager Methods
+    def create_secret(self, name, secret_value, description=None, tags=None):
+        return self.aws_manager.SecretsManager.create_secret(name=name, secret_value=secret_value, 
+                                                          description=description, tags=tags)
+
+    def get_secret_value(self, secret_id, version_id=None, version_stage=None):
+        return self.aws_manager.SecretsManager.get_secret_value(secret_id=secret_id,
+                                                             version_id=version_id,
+                                                             version_stage=version_stage)
+
+    def update_secret(self, secret_id, secret_value, description=None):
+        return self.aws_manager.SecretsManager.update_secret(secret_id=secret_id,
+                                                          secret_value=secret_value,
+                                                          description=description)
+
+    def delete_secret(self, secret_id, force_delete=False, recovery_window_days=30):
+        return self.aws_manager.SecretsManager.delete_secret(secret_id=secret_id,
+                                                          force_delete=force_delete,
+                                                          recovery_window_days=recovery_window_days)
+
+    def list_secrets(self, filters=None, max_results=100):
+        return self.aws_manager.SecretsManager.list_secrets(filters=filters,
+                                                         max_results=max_results)
+
 class ServiceParameters(BaseComponent):
     def service_parameters(self,pane,datapath=None,**kwargs):
         fb = pane.formbuilder(datapath=datapath)
