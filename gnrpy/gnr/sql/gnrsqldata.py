45437165d5637674b83df1e8da1918bec3fa2a4c
Michele Bertoldi <michele.bertoldi@softwell.it>
2025-02-24 19:03:02 +0100

    Fix path construction in getFieldAlias for correct relation handling

--- gnrpy/gnr/sql/gnrsqldata.py
+++ gnrpy/gnr/sql/gnrsqldata.py
@@ -217,8 +217,8 @@ class SqlQueryCompiler(object):
                 #pathlist.append(fldalias.relation_path)
                 #newfieldpath = '.'.join(pathlist)        # replace the field alias with the column relation_path
                 # then call getFieldAlias again with the real path
-                return self.getFieldAlias(fldalias.relation_path, curr=curr,
-                                          basealias=alias, parent='.'.join(pathlist))  # call getFieldAlias recursively
+                return self.getFieldAlias(f"{'.'.join(pathlist)}.{fldalias.relation_path}", #curr=curr,
+                                          basealias=basealias), #parent='.'.join(pathlist))  # call getFieldAlias recursively
             elif fldalias.sql_formula or fldalias.select or fldalias.exists:
                 sql_formula = fldalias.sql_formula
                 attr = dict(fldalias.attributes)
