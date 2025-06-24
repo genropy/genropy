a1f9d147f4a4263cc5adfc38a587f69826e0d119
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2025-06-24 11:42:20 +0200

    Introducing base class for database related tests, and updated package tester to use it.

--- gnrpy/gnr/dev/packagetester.py
+++ gnrpy/gnr/dev/packagetester.py
@@ -1,20 +1,5 @@
-import os
-from gnr.web.gnrwsgisite import GnrWsgiSite
-from gnr.lib.services import ServiceHandler
+from gnr.dev.testbase import DbBasedTest
 
-class PackageTester(object):
-    @classmethod
-    def setup_class(cls):
-        cls.instance_name = os.environ.get("GNR_TESTING_INSTANCE_NAME")
-        cls.site = GnrWsgiSite(cls.instance_name)
-        cls.app = cls.site.db.application
-
-    @classmethod
-    def _get_base_service(cls, service_type, service_implementation,
-                          service_name, **kwargs):
-        sh = ServiceHandler(cls.site)
-        service = sh.service_types[service_type]
-        return service.implementations[service_implementation](cls.site, **kwargs)
-    
-
+class PackageTester(DbBasedTest):
+    pass
     
