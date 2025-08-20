b995abce5b98d2c44d4168afb620bddf4f2d24bf
Davide Paci <davide.paci@softwell.it>
2025-08-20 08:43:18 +0200

    No check config

--- resources/js_libs/ckeditor/config.js
+++ resources/js_libs/ckeditor/config.js
@@ -4,6 +4,9 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+
+	config.versionCheck = false;
+
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
