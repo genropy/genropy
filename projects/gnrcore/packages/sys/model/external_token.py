c9f7f22ca36342ef92cd59c5f1895159c24c735d
fporcari <francesco.porcari@softwell.it>
2025-10-07 18:14:16 +0200

    fix token

--- projects/gnrcore/packages/sys/model/external_token.py
+++ projects/gnrcore/packages/sys/model/external_token.py
@@ -49,9 +49,15 @@ class Table(object):
                 parameters=Bag(parameters))
         self.insert(record)
         return record['id']
-
+    
+    def get_storeargs(self):
+        storeargs = {}
+        if not self.db.multidomain:
+            storeargs = {'storename':self.db.rootstore}
+        return storeargs
+    
     def use_token(self, token, host=None):
-        with self.db.tempEnv(connectionName='system',storename=self.db.rootstore):
+        with self.db.tempEnv(connectionName='system',**self.get_storeargs()):
             record = self.record(id=token, ignoreMissing=True).output('bag')
             record = self.check_token(record, host)
             if record:
