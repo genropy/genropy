60504490b578052d15e878c431e881fa35c264ab
fporcari <francesco.porcari@softwell.it>
2025-10-07 14:19:06 +0200

    message_to_send

---
+++ projects/gnrcore/packages/email/model/message_to_send.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+class Table(object):
+    def config_db(self,pkg):
+        tbl=pkg.table('message_to_send', pkey='message_id', 
+                      name_long='[en]Sending messages',order_by='$__ins_ts')
+        self.sysFields(tbl,id=False)
+        tbl.column('dbstore', name_long='Dbstore')
+        tbl.column('message_id',size='22', group='_', name_long='Message'
+                    ).relation('message.id',one_one=True,
+                               relation_name='sending_index')    
+
+    def addMessageToQueue(self,message_id):
+        dbstore = self.db.currentEnv.get('storename')
+        with self.db.tempEnv(storename=False):
+            self.insert(self.newrecord(message_id=message_id,dbstore=dbstore))
+
+    def removeMessageFromQueue(self,message_id):
+        with self.db.tempEnv(storename=False):
+            self.deleteSelection('message_id',message_id)
+
+    def sendMessages(self):
+        with self.db.tempEnv(storename=False):
+            messages_to_send = [r['message_id'] for r in self.query().fetchGroped('dbstore')]
+        for dbstore,message_pkeys in messages_to_send.items():
+            with self.db.tempEnv(storename=dbstore or False):
+                for message_id in message_pkeys:
+                    self.message_tbl.sendMessage(pkey=message_id)
