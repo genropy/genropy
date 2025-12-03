774a35e007371224c41aac5250b38ae3dfcef20c
fporcari <francesco.porcari@softwell.it>
2025-12-03 17:18:18 +0100

    remove unused import

--- gnrpy/tests/app/test_gnrsqlappdb.py
+++ gnrpy/tests/app/test_gnrsqlappdb.py
@@ -2,7 +2,7 @@ import tempfile
 
 from gnr.core.gnrbag import Bag
 from gnr.app.gnrapp import GnrSqlAppDb, DbStoresHandler
-from gnrpy.tests.sql.common import MockApplication, MockCache
+from gnrpy.tests.sql.common import MockApplication
 
 
 class TestGnrSqlAppDb:
