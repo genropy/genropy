b3c310f2bf6ff41cc2c535adf2b1635bb88a697d
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2026-02-17 12:52:54 +0100

    let flake8 checks pass in order to have all the tests passing

--- projects/gnrcore/packages/sys/webpages/ep_task.py
+++ projects/gnrcore/packages/sys/webpages/ep_task.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 from gnr.core.gnrdecorator import public_method
-from gnr.core.gnrbag import Bag
+
 
 class GnrCustomWebPage(object):
     py_requires='gnrcomponents/externalcall:BaseRpc'
