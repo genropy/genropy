0fb1ea53b3290acbd67c27a9c891ec8d9b6c9a3a
fporcari <francesco.porcari@softwell.it>
2026-02-27 02:16:16 +0100

    test(grouplet): add view_store__onStart=True to all dialogTableHandler tests
    Enable automatic store loading on start for all dialogTableHandler
    instances in grouplet navigation tests, and simplify standalone test
    forms to use plain borderContainer patterns.

--- projects/gnrcore/packages/test/webpages/gnrwdg/test_grouplets/06_grouplet_navigation.py
+++ projects/gnrcore/packages/test/webpages/gnrwdg/test_grouplets/06_grouplet_navigation.py
@@ -19,29 +19,17 @@ class GnrCustomWebPage(object):
             table='test.myprospect',
             datapath='.prospect_tree',
             viewResource='View',
-            formResource='Form')
+            formResource='Form',
+            view_store__onStart=True)
 
     def test_2_panel_multibutton_table(self, pane):
         """groupletPanel with topic and table: multiButton selector for technical grouplets"""
-        form = pane.frameForm(frameCode='panel_mb_form',
-                             border='1px solid silver',
-                             datapath='.panel_mb',
-                             height='500px',
-                             _anchor=True,
-                             pkeyPath='.ticket_pkey')
-        form.formStore(table='test.myticket', storeType='Item',
-                      handler='recordCluster', startKey='*norecord*')
-        bar = form.top.slotToolbar('5,selector,*,semaphore,5')
-        fb = bar.selector.formbuilder(cols=1, border_spacing='1px')
-        fb.dbselect(value='^.ticket_pkey', dbtable='test.myticket',
-                   parentForm=False,
-                   validate_onAccept="if(userChange){this.getParentNode().form.publish('load',{destPkey:value})}",
-                   lbl='Ticket')
-        form.center.contentPane().groupletPanel(
+        pane.borderContainer(height='500px', border='1px solid silver',
+                         datapath='.panel_mb').groupletPanel(
             table='test.myticket',
             topic='technical',
-            value='^.record.extra_data',
-            frameCode='mb_table_panel')
+            value='^.data',
+            frameCode='mb_table_panel', region='center')
 
     def test_3_panel_multibutton_no_table(self, pane):
         """groupletPanel with topic, no table: multiButton selector for app grouplets"""
@@ -60,7 +48,8 @@ class GnrCustomWebPage(object):
             table='test.booking',
             datapath='.booking',
             viewResource='View',
-            formResource='Form')
+            formResource='Form',
+            view_store__onStart=True)
 
     def test_5_wizard_standalone(self, pane):
         """Standalone wizard with app topic, no external form"""
@@ -77,35 +66,23 @@ class GnrCustomWebPage(object):
             table='test.myprospect',
             datapath='.prospect_collapsible',
             viewResource='View',
-            formResource='FormGrid')
+            formResource='FormGrid',
+            view_store__onStart=True)
 
     # --- Menu navigation ---
 
     def test_7_menu_tree(self, pane):
-        """Select a comune, load record, navigate grouplets via tree"""
-        form = pane.frameForm(frameCode='comune_grp_menu',
-                             border='1px solid silver',
-                             datapath='.comune_form',
-                             height='500px',
-                             _anchor=True,
-                             pkeyPath='.comune_pkey')
-        form.formStore(table='glbl.comune', storeType='Item',
-                      handler='recordCluster', startKey='*norecord*')
-        bar = form.top.slotToolbar('5,selector,*,semaphore,5')
-        fb = bar.selector.formbuilder(cols=1, border_spacing='1px')
-        fb.dbselect(value='^.comune_pkey', dbtable='glbl.comune',
-                   parentForm=False,
-                   validate_onAccept="if(userChange){this.getParentNode().form.publish('load',{destPkey:value})}",
-                   lbl='Comune')
-
-        bc = form.center.borderContainer()
+        """Navigate grouplets via tree menu: click a grouplet caption in the tree
+        to render it in the center panel."""
+        bc = pane.borderContainer(height='500px', border='1px solid silver',
+                                  datapath='.comune_menu')
 
         left = bc.contentPane(region='left', width='220px',
                               splitter=True, border_right='1px solid silver',
                               padding='10px')
         left.dataRpc('.grouplets_menu', self.gr_getGroupletMenu,
                        table='glbl.comune', _onStart=True)
-        left.tree(storepath='#FORM.grouplets_menu',
+        left.tree(storepath='.grouplets_menu',
                   hideValues=True,
                   labelAttribute='caption',
                   selectedLabelClass='selectedTreeNode',
@@ -115,73 +92,59 @@ class GnrCustomWebPage(object):
                   """,
                   connect_onClick="""
                       if($2.item.attr.resource && $2.item.attr.grouplet_caption){
-                          SET #FORM.selected.resource = $2.item.attr.resource;
-                          SET #FORM.selected.caption = $2.item.attr.grouplet_caption;
+                          SET .selected_resource = $2.item.attr.resource;
+                          SET .selected_caption = $2.item.attr.grouplet_caption;
                       }
                   """)
 
         right = bc.borderContainer(region='center')
         top = right.contentPane(region='top', height='30px',
                                 padding='5px', border_bottom='1px solid silver')
-        top.div('^#FORM.selected.caption',
+        top.div('^.selected_caption',
                 font_weight='bold', font_size='1.2em')
         right.contentPane(region='center', overflow='auto').grouplet(
-            resource='^#FORM.selected.resource',
+            resource='^.selected_resource',
             table='glbl.comune',
-            value='^.record')
+            value='^.data')
 
     def test_8_menu_topic_filter(self, pane):
-        """Grouplet menu filtered by topic: loads only grouplets within 'technical'"""
-        form = pane.frameForm(frameCode='ticket_topic_menu',
-                             border='1px solid silver',
-                             datapath='.ticket_topic_form',
-                             height='500px',
-                             _anchor=True,
-                             pkeyPath='.ticket_pkey')
-        form.formStore(table='test.myticket', storeType='Item',
-                      handler='recordCluster', startKey='*norecord*')
-        bar = form.top.slotToolbar('5,selector,*,semaphore,5')
-        fb = bar.selector.formbuilder(cols=1, border_spacing='1px')
-        fb.dbselect(value='^.ticket_pkey', dbtable='test.myticket',
-                   parentForm=False,
-                   validate_onAccept="if(userChange){this.getParentNode().form.publish('load',{destPkey:value})}",
-                   lbl='Ticket')
-
-        bc = form.center.borderContainer()
+        """Grouplet menu filtered by topic: tree shows only grouplets within 'technical'"""
+        bc = pane.borderContainer(height='500px', border='1px solid silver',
+                                  datapath='.ticket_topic_menu')
 
         left = bc.contentPane(region='left', width='220px',
                               splitter=True, border_right='1px solid silver',
                               padding='10px')
         left.dataRpc('.topic_menu', self.gr_getGroupletMenu,
                        table='test.myticket', topic='technical', _onStart=True)
-        left.tree(storepath='#FORM.topic_menu',
+        left.tree(storepath='.topic_menu',
                   hideValues=True,
                   labelAttribute='caption',
                   selectedLabelClass='selectedTreeNode',
                   openOnClick=True,
                   connect_onClick="""
                       if($2.item.attr.resource && $2.item.attr.grouplet_caption){
-                          SET #FORM.selected.resource = $2.item.attr.resource;
-                          SET #FORM.selected.caption = $2.item.attr.grouplet_caption;
+                          SET .selected_resource = $2.item.attr.resource;
+                          SET .selected_caption = $2.item.attr.grouplet_caption;
                       }
                   """)
 
         right = bc.borderContainer(region='center')
         top = right.contentPane(region='top', height='30px',
                                 padding='5px', border_bottom='1px solid silver')
-        top.div('^#FORM.selected.caption',
+        top.div('^.selected_caption',
                 font_weight='bold', font_size='1.2em')
         right.contentPane(region='center', overflow='auto').grouplet(
-            resource='^#FORM.selected.resource',
+            resource='^.selected_resource',
             table='test.myticket',
-            value='^.record')
+            value='^.data')
 
     def test_9_menu_multibutton(self, pane):
         """Non-table grouplet menu with topic: multiButton selection from grouplets/app"""
         frame = pane.framePane(frameCode='app_grouplet_frame',
                                height='500px', border='1px solid silver',
                                datapath='.app_grouplet')
-        bar = frame.top.slotBar('*,mb,*',_class='mobile_bar')
+        bar = frame.top.slotBar('*,mb,*', _class='mobile_bar')
         bar.dataRpc('.app_menu', self.gr_getGroupletMenu,
                     topic='app', _onStart=True)
         bar.mb.multibutton(value='^.selected_code', storepath='.app_menu')
@@ -202,4 +165,5 @@ class GnrCustomWebPage(object):
             table='test.myticket',
             datapath='.tickets',
             viewResource='View',
-            formResource='Form')
+            formResource='Form',
+            view_store__onStart=True)
