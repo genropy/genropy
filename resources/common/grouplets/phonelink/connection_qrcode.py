7ea9fa230fa7cb3f39f457cdf4d233a2e7db7f7c
fporcari <francesco.porcari@softwell.it>
2026-03-03 16:43:39 +0100

    style(phonelink): fix cosmetic issues and align __info__ pattern
    Fix double spaces, missing spaces after commas in __info__ dicts.
    Align connection_qrcode.__info__ to same pattern as android/ios
    (notEnabled = not ...; tags='notEnabled' if notEnabled else None).

--- resources/common/grouplets/phonelink/connection_qrcode.py
+++ resources/common/grouplets/phonelink/connection_qrcode.py
@@ -3,10 +3,9 @@ from gnr.web.gnrbaseclasses import BaseComponent
 
 class Grouplet(BaseComponent):
     def __info__(self):
+        notEnabled = not self.site.is_mobile_app_enabled()
         return dict(code='connection_qrcode', caption='!![en]Connection', priority=3,
-                    tags=None if self.site.is_mobile_app_enabled()  else 'notEnabled')
-
-
+                    tags='notEnabled' if notEnabled else None)
 
     def grouplet_main(self, pane, **kwargs):
         pane.dataController(""" const qrcode_text = `GENRO:${owner_name || sitename}:${url}`;
