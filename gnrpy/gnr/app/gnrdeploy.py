ef0e4c8e98de585c0126956b7fc7c967bd99c008
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2026-03-11 16:38:23 +0100

    complete porting of duplicate code from gnrmkthresource cli script to ThPackageResourceMaker in gnrdeploy.
    refs #102

--- gnrpy/gnr/app/gnrdeploy.py
+++ gnrpy/gnr/app/gnrdeploy.py
@@ -894,15 +894,12 @@ class ThPackageResourceMaker(object):
         self.has_lookups = False
         
     def makeResources(self):
-        menu_xml_path = os.path.join(self.packageFolder, 'menu.xml')
-        self.pkg_menus[self.package] = Bag(menu_xml_path) if os.path.exists(menu_xml_path) else Bag()
-        print("BAU")
         for table in self.tables:
             table_full_name = f"{self.package}.{table}"
             logger.debug("Processing table %s", table_full_name)
             if 'lookup' in self.app.db.table(table_full_name).attributes:
                 logger.debug("Skipping lookup table %s", table_full_name)
-                self.has_lookup = True
+                self.has_lookups = True
                 continue
             self.createResourceFile(table)
 
@@ -910,8 +907,16 @@ class ThPackageResourceMaker(object):
             self.makeMenu()
 
     def makeMenu(self):
-        hasLookups = False
+        menu_xml_path = os.path.join(self.packageFolder, 'menu.xml')
+        self.pkg_menus[self.package] = Bag(menu_xml_path) if os.path.exists(menu_xml_path) else Bag()
+
+        if self.has_lookups:
+            self.pkg_menus[self.package].setItem('auto.lookups', None,
+                                                 label='!!Lookup tables',
+                                                 pkg=self.package, tag='lookupBranch')
+                                                 
         menupath = Path(self.packageFolder) / 'menu.py'
+        
         if menupath.exists():
             logger.info("Menu file %s exists, appending", menupath)
             source = menupath.read_text(encoding="utf-8")
@@ -975,13 +980,14 @@ class ThPackageResourceMaker(object):
             for t in self.tables:
                 tblobj = self.app.db.table('%s.%s' %(self.package,t))
                 if tblobj.attributes.get('lookup'):
-                    hasLookups = True
+                    self.has_lookups = True
                 else:
                     m.thpage(tblobj.name_plural or tblobj.name_long, table=tblobj.fullname)
-            if hasLookups:
+            if self.has_lookups:
                 m.lookupBranch("Lookup tables", pkg=self.package)
             m.toPython(menupath)
-
+        if os.path.exists(menu_xml_path):
+            os.remove(menu_xml_path)
     def write(self,line=None, indent=0):
         line = line or ''
         self.out_file.write('%s%s\n'%(self.option_indent*indent*' ',line))
@@ -995,16 +1001,25 @@ class ThPackageResourceMaker(object):
         self.write("from gnr.web.gnrbaseclasses import BaseComponent")
         self.write()
     
-    def writeViewClass(self, tblobj, columns):
+    def writeViewClass(self, tblobj, column_groups):
         self.write("class View(BaseComponent):")
         self.write()
         self.write("def th_struct(self,struct):", indent=1)
         self.write('r = struct.view().rows()', indent=2)
-        for column, size, dtype in columns:
-            if self.option_guess_size:
-                self.write(f"r.fieldcell('{column}', width='{size}em')", indent=2)
+        for group, columns in column_groups.items():
+            if group:
+                self.write(out_file, f"{group[0]}_col_group = r.columnset('{group[0]}', name='{group[1]}')", indent = 2)
+                for column, size in columns:
+                    if self.option_guess_size:
+                        self.write(out_file, "%s_col_group.fieldcell('%s', width='%iem')" % (group[0], column, size), indent=2)
+                    else:
+                        self.write(out_file, "%s_col_group.fieldcell('%s')" % (group[0], column), indent=2)
             else:
-                self.write(f"r.fieldcell('{column}')", indent=2)
+                for column, size in columns:
+                    if self.option_guess_size:
+                        self.write(out_file, "r.fieldcell('%s', width='%iem')"%(column,size), indent=2)
+                    else:
+                        self.write(out_file, "r.fieldcell('%s')"%column, indent=2)
         self.write()
         self.write("def th_order(self):", indent=1)
         self.write("return '%s'" % columns[0][0], indent=2)
@@ -1068,6 +1083,140 @@ class ThPackageResourceMaker(object):
                 result.append((relation,j.get('thmode')))
         return result
 
+    def columnWidthEstimate(self, column):
+        """
+        Estimates the width of a column based on its data type (dtype) and specified size attributes.
+        
+        Description
+        -----------
+        For text columns ('A' or 'T'), it uses a conversion map
+        to determine the appropriate width. For date ('D') and datetime ('DH') columns, it returns
+        fixed widths. If the data type is not recognized, it returns a default width.
+
+        Parameters
+        ----------
+        column : Column
+            The column object containing attributes such as dtype and size.
+
+        Returns
+        -------
+        int
+            The calculated width of the column. 
+        """
+        # TODO: Review the LUT and evaluate the use of a formula.
+        # Note that the commented formulae seems to be less accurate than the LUT
+
+        MIN_WIDTH = 2
+        MAX_SIZE = 50
+        DEFAULT_WIDTH = 7
+
+        sizeWidthMap = {
+            1: 2,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 4,
+            6: 5,
+            7: 5,
+            8: 6,
+            9: 6,
+            10: 6,
+            11: 6,
+            12: 7,
+            13: 7,
+            14: 8,
+            15: 8,
+            16: 9,
+            17: 9,
+            18: 10,
+            19: 10,
+            20: 10,
+            21: 11,
+            22: 11,
+            23: 11,
+            24: 12,
+            25: 12,
+            26: 12,
+            27: 13,
+            28: 13,
+            29: 14,
+            30: 14,
+            31: 15,
+            32: 15,
+            33: 15,
+            34: 16,
+            35: 17,
+            36: 17,
+            37: 18,
+            38: 18,
+            39: 18,
+            40: 19,
+            41: 19,
+            42: 20,
+            43: 20,
+            44: 21,
+            45: 21,
+            46: 22,
+            47: 22,
+            48: 23,
+            49: 23,
+            50: 24
+        }
+
+        def handleTextColumn(column):
+            try:
+                sizeTxt = column.attributes.get('size', '')
+            except Exception as e:
+                logger.warning(f'Unable to get size attribute for column {column.name}: {e}. Using default width.')
+                return DEFAULT_WIDTH
+            
+            if not sizeTxt:
+                logger.warning(f'No size attribute found for column {column.name}. Using default width.')
+                return DEFAULT_WIDTH
+
+            if ':' in sizeTxt:
+                size = int(sizeTxt.split(':')[1])
+            else:
+                size = int(sizeTxt)
+
+            if size > max(sizeWidthMap):
+                logger.info(f'Column {column.name} has a size greater than the maximum. Topping to max handled value.')
+                return sizeWidthMap[MAX_SIZE]
+            # LUT conversion
+            return sizeWidthMap[size]
+            
+            # ChatGPT 4o conversion
+            # return round(0.429 * size + 1.709)
+
+            # ChatGPT o3-mini-high conversion
+            # return round(0.45 * size + 1.55)
+
+            # Deepseek conversion
+            # return round(0.44 * size + 1.56)
+
+            # Claude conversion
+            # return floor(2.8 * size^0.45)
+
+        typesHandler = {
+            'A': handleTextColumn,  # Varchar
+            'T': handleTextColumn,  # Text
+            'C': handleTextColumn,  # Chars
+            'D': 6,       # Date
+            'H': 4,       # Time
+            'DH': 9,      # DateTime
+            'DHZ': 12,    # DateTime + TimeZone
+            'B': 3,       # Boolean
+            'N': 7,       # Numeric
+            'L': 7,       # Numeric (long)
+            'I': 7,       # Numeric (int)
+            'R': 7,       # Numeric (float)
+            'X': 10,      # Bag
+        }
+        handler = typesHandler.get(column.dtype, 7)
+        if callable(handler):
+            return handler(column)
+        return handler
+
     def createResourceFile(self, table):
         resourceFolder = os.path.join(self.packageFolder,'resources', 'tables', table)
         if not os.path.exists(resourceFolder):
@@ -1080,37 +1229,48 @@ class ThPackageResourceMaker(object):
         
         column_groups = defaultdict(list)
         columns = []
-        view_columns=[]
-        form_columns=[]
-        max_size = 35
         tbl_obj =  self.app.db.table('%s.%s'%(self.package,table))
-        for col_name,column in list(tbl_obj.columns.items()):
-            dtype = column.dtype
-            if column.attributes.get('_sysfield') or dtype =='O': 
-                continue
-            if column.dtype=='A':
-                size = column.attributes.get('size','')
-                if size:
-                    if ':' in size:
-                        size =size.split(':')[1]
-                else:
-                    size=max_size
-                size = max(int(size),max_size)
-            else:
-                size = 7
-            if dtype!='X' or self.bag_columns['view']:
-                view_columns.append((column.name,size,dtype))
-            if dtype!='X' or self.bag_columns['form']:
-                form_columns.append((column.name,size,dtype))
-            
 
-        with open(path,'w') as out_file:
-            self.out_file = out_file
-            self.writeHeaders()
-            self.writeImports()
-            self.writeViewClass(tbl_obj, view_columns)
-            self.writeFormClass(tbl_obj, form_columns)
-            logger.info('%s created', name)
+        for col_name, column in tbl_obj.columns.items():
+
+            # Skip id and internal columns
+            if col_name=='id' or column.attributes.get('_sysfield') or col_name.startswith('__') or column.type == 'O':
+                continue
+
+            logger.debug(f'Processing column {column.name}')
+            if self.option_guess_size:
+                width = self.columnWidthEstimate(column)
+                logger.debug(f'Estimated width for column {column.name}: {width}em')
+            else:
+                width = 7
+
+            col_group_label = column.attributes.get("colgroup_label", None)
+
+            if col_group_label is None:
+                col_group = ""
+            else:
+                col_group = (
+                    col_group_label,
+                    column.attributes.get("colgroup_name_long")
+                )
+
+            column_groups[col_group].append((column.name,width))
+            columns.append((column.name, width))
+
+        if not columns:
+            logger.error("Table %s does not contain any valid column", table)
+            return
+
+        try:
+            with open(path,'w') as out_file:
+                self.out_file = out_file
+                self.writeHeaders()
+                self.writeImports()
+                self.writeViewClass(tbl_obj, column_groups)
+                self.writeFormClass(tbl_obj, columns)
+                logger.info('%s created', name)
+        except Exception as e:
+            logger.exception(f'Error creating output file: {path}. Error: {e}')
 
 ################################# DEPLOY CONF BUILDERS ################################
 
