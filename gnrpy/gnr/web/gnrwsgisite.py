9e25cfc4c639bde246d778f7b81b555cc0a470a7
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2026-03-13 19:31:55 +0100

    move the DataCollector (object that analyze a register) from the CLI (#697)
    script into a separate module into gnrwsgisite_proxy, where the
    current register resides. The collector is available via site object.
    
    Update CLI and prometheus webtool accordingly.

--- gnrpy/gnr/web/gnrwsgisite.py
+++ gnrpy/gnr/web/gnrwsgisite.py
@@ -41,6 +41,8 @@ from gnr.web.gnrwsgisite_proxy.gnrstatichandler import StaticHandlerManager
 from gnr.web.gnrwsgisite_proxy.gnrpwahandler import PWAHandler
 from gnr.web.gnrwsgisite_proxy.gnrsiteregister import SiteRegisterClient, DEFAULT_PAGE_MAX_AGE
 from gnr.web.gnrwsgisite_proxy.gnrwebsockethandler import WsgiWebSocketHandler
+from gnr.web.gnrwsgisite_proxy.datacollector import DataCollector
+
 from gnr.web.gnrwebreqresp import GnrWebRequest
 try:
     from werkzeug import EnvironBuilder
@@ -505,6 +507,8 @@ class GnrWsgiSite(object):
         # is not initialized, since self.register is a property
         # and it initialze the register itself.
         self.register
+        
+        self.datacollector = DataCollector(self.register.siteregister)
         
         if counter == 0 and self.debug:
             self.onInited(clean=not noclean)
