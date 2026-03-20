genropy/genropy

commit 2222f99db458fec220936dc24efff75f8cb55819

feat(#677): centralized errorHandler with pkgBroadcast, toast notifications, and traceback viewer

gnrpy/gnr/web/gnrwsgisite.py
@@ -9,7 +9,7 @@ import mimetypes
 import functools
 from time import time
 from collections import defaultdict
-from threading import RLock
+from threading import RLock, Thread
 import warnings
 
 import requests
@@ -22,8 +22,8 @@ from werkzeug.exceptions import (HTTPException, InternalServerError,
 from gnr.core.gnrbag import Bag
 from gnr.core import gnrstring
 from gnr.core.gnrlang import GnrException, GnrDebugException
-from gnr.core.gnrlang import tracebackBag, getUuid, ThreadedDict
-from gnr.core.gnrdecorator import public_method, deprecated
+from gnr.core.gnrlang import getUuid, ThreadedDict
+from gnr.core.gnrdecorator import deprecated
 from gnr.core.gnrconfig import getGnrConfig,getEnvironmentItem
 from gnr.core.gnrsys import expandpath
 from gnr.core.gnrstring import boolean
@@ -1004,27 +1004,48 @@ class GnrWsgiSite(object):
         response = requests.post(url, headers=headers, data=kwargs)
         return self.gnrapp.catalog.fromTypedText(response.text)
 
-    def writeException(self, exception=None, traceback=None):
-        try:
-            page = self.currentPage
-            user, user_ip, user_agent = (page.user, page.user_ip, page.user_agent) if page else (None, None, None)
-            return self.db.table('sys.error').writeException(description=str(exception),
-                                                      traceback=traceback,
-                                                      user=user,
-                                                      user_ip=user_ip,
-                                                      user_agent=user_agent)
-        except Exception as writingErrorException:
-            logger.exception('\n ####writingErrorException %s for exception %s' %(str(writingErrorException),str(exception)))
+    def errorHandler(self, exception=None, **kwargs):
+        page = self.currentPage
+        if page:
+            kwargs.setdefault('user', page.user)
+            kwargs.setdefault('user_ip', page.user_ip)
+            kwargs.setdefault('user_agent', page.user_agent)
+            kwargs.setdefault('page_id', getattr(page, 'page_id', None))
+            request = getattr(page, 'request', None)
+            if request:
+                kwargs['request_uri'] = request.url
+                kwargs['request_host'] = request.host_url
 
-    @public_method
-    def writeError(self, description=None,error_type=None, **kwargs):
+        kwargs.setdefault('current_domain', self.currentDomain)
+        error_id = self.gnrapp.errorHandler(exception=exception, **kwargs)
+        if error_id and page:
+            notify_user = kwargs.get('notify_user')
+            if notify_user:
+                try:
+                    if notify_user is True:
+                        notify_user = str(exception)
+                    msg = '%s (ref: %s)' % (notify_user, error_id)
+                    page.setInClientData('gnr.server_error', msg, fired=True)
+                except Exception:
+                    logger.debug('Failed to notify user of error %s', error_id)
+        if error_id:
+            self._sendToErrorEndpoints(error_id, kwargs)
+        return error_id
+
+    def _sendToErrorEndpoints(self, error_id, error_info):
+        endpoint = self.config['error_handler?error_endpoint']
+        if not endpoint:
+            return
+        error_info['error_id'] = error_id
+        Thread(target=self._postToEndpoint,
+               args=(endpoint, error_info), daemon=True).start()
+
+    def _postToEndpoint(self, endpoint, error_info):
         try:
-            page = self.currentPage
-            user, user_ip, user_agent = (page.user, page.user_ip, page.user_agent) if page else (None, None, None)
-            self.db.table('sys.error').writeError(description=description,error_type=error_type,user=user,user_ip=user_ip,user_agent=user_agent,**kwargs)
-        except Exception as e:
-            logger.exception(str(e))
-            pass
+            requests.post(endpoint, json=error_info, timeout=5)
+        except Exception:
+            logger.warning('Failed to send error to endpoint %s', endpoint)
+
 
     def loadResource(self, pkg, *path):
         """TODO
@@ -1068,7 +1089,10 @@ class GnrWsgiSite(object):
                     else:
                         out_dict[name] = value
                 except UnicodeDecodeError:
-                    pass
+                    self.errorHandler(
+                        description='UnicodeDecodeError parsing request parameter',
+                        loglevel='warning', origin='data'
+                    )
         return out_dict
 
     @property
@@ -1130,16 +1154,21 @@ class GnrWsgiSite(object):
         try:
             return self._dispatcher(environ, start_response)
         except Exception as e:
-            page = self.currentPage
-            if self.debug and ((page and page.isDeveloper()) or self.force_debug):
-                raise
-            self.writeException(exception=e,traceback=tracebackBag())
+            self.raiseIfDeveloper()
+            self.errorHandler(exception=e, traceback=True)
             exc = InternalServerError(
                 description='Internal server error; SCRIPT_NAME=%r; PATH_INFO=%r;'
                 % (environ.get('SCRIPT_NAME'), environ.get('PATH_INFO')))
             return exc(environ, start_response)
         finally:
             self.currentDomain = self.rootDomain
+
+    def raiseIfDeveloper(self, exception=None):
+        page = self.currentPage
+        if self.debug and ((page and page.isDeveloper()) or self.force_debug):
+            if exception is not None:
+                raise exception
+            raise
 
     @property
     def external_host(self):

projects/gnrcore/packages/sys/model/error.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from gnr.core.gnrbag import Bag
 
 class Table(object):
     def config_db(self, pkg):
@@ -21,21 +20,39 @@ class Table(object):
 
         tbl.column('fixed',name_long='Fixed')
         tbl.column('notes',name_long='Notes')
-        tbl.column('error_type',name_long='!!Error type',values='ERR:Error,EXC:Exception')
+        tbl.column('error_type',name_long='!!Error type')
+        tbl.column('error_code',name_long='!!Error code',indexed=True)
+        tbl.formulaColumn('detail_url',
+                          """(CASE WHEN $request_host IS NULL THEN '/sys/ep_error?error_id=' || $id
+                                ELSE $request_host || '/sys/ep_error?error_id=' || $id
+                             END)
+                            """,
+                          name_long='!!Detail')
+        tbl.column('request_uri',name_long='!!Request URI')
+        tbl.column('request_host',name_long='!!Request Host')
+
+        tbl.column('rpc_method',name_long='!!RPC Method')
+        tbl.column('rpc_kwargs',dtype='X',name_long='!!RPC kwargs')
+        tbl.column('page_id',name_long='!!Page ID',size='22')
+        tbl.column('current_domain',name_long='!!Current domain')
 
 
-    def writeException(self,description=None,traceback=None,user=None,user_ip=None,user_agent=None):
-        rec = dict(description=description,error_data=traceback,username=user,user_ip=user_ip,user_agent=user_agent,error_type='EXC')
-        with self.db.tempEnv(connectionName='system',storename=self.db.rootstore):
+    def errorHandler(self, error_id=None, description=None, traceback=None,
+                     error_type=None, user=None, user_ip=None,
+                     user_agent=None, request_uri=None,
+                     rpc_method=None, rpc_kwargs=None, page_id=None,
+                     request_host=None, current_domain=None, **kwargs):
+        rec = dict(error_code=error_id, description=description,
+                   error_data=traceback, error_type=error_type,
+                   username=user, user_ip=user_ip,
+                   user_agent=user_agent, request_uri=request_uri,
+                   request_host=request_host,
+                   rpc_method=rpc_method, rpc_kwargs=rpc_kwargs,
+                   page_id=page_id,
+                   current_domain=current_domain)
+        with self.db.tempEnv(connectionName='system',
+                             storename=self.db.rootstore):
             self.insert(rec)
             self.db.commit()
         return rec
 
-    def writeError(self,description=None,error_type=None,user=None,user_ip=None,user_agent=None,**kwargs):
-        error_data = Bag(self.db.currentEnv)
-        error_data.update(kwargs)
-        rec = dict(description=description,error_data=error_data,username=user,user_ip=user_ip,user_agent=user_agent,error_type=error_type or 'ERR')
-        with self.db.tempEnv(connectionName='system',storename=self.db.rootstore):
-            self.insert(rec)
-            self.db.commit()
-        return rec

resources/common/th/th_form.py
@@ -20,7 +20,9 @@ class TableHandlerForm(BaseComponent):
     def th_tableEditor(self,pane,frameCode=None,table=None,th_pkey=None,formResource=None,
                         formInIframe=False,dfltoption_kwargs=None,**kwargs):
         table = table or pane.attributes.get('table')
-        resourcePath = self._th_mixinResource(frameCode,table=table,resourceName=formResource,defaultClass='Form') 
+        resourcePath = self._th_mixinResource(frameCode,table=table,resourceName=formResource,defaultClass='Form',pane=pane)
+        if not resourcePath:
+            return
         options = dfltoption_kwargs
         options.update(self._th_getOptions(frameCode))
         options.update(kwargs)
@@ -131,7 +133,9 @@ class TableHandlerForm(BaseComponent):
                         store='recordCluster',handlerType=None,tree_kwargs=None,**kwargs):
         tableCode = table.replace('.','_')
         formId = formId or tableCode
-        resourcePath = self._th_mixinResource(formId,table=table,resourceName=formResource,defaultClass='Form')
+        resourcePath = self._th_mixinResource(formId,table=table,resourceName=formResource,defaultClass='Form',pane=pane)
+        if not resourcePath:
+            return
         resource_options = self._th_getOptions(formId)
         resource_options.update(kwargs)
         resource_options.update(tree_kwargs)

resources/common/th/th_lib.py
@@ -5,12 +5,20 @@
 # Copyright (c) 2011 Softwell. All rights reserved.
 
 
-from gnr.web.gnrwebpage import BaseComponent
+from gnr.web.gnrwebpage import BaseComponent, GnrMissingResourceException
 from gnr.core.gnrbag import Bag
 from gnr.core.gnrdecorator import public_method
+from gnr.core.gnrclasses import GnrMixinNotFound
 
 
 class TableHandlerCommon(BaseComponent):
+
+    def _th_missingResource(self, pane, exception):
+        self.site.raiseIfDeveloper(exception=exception)
+        self.site.errorHandler(exception=exception, error_type='missing_resource',
+            notify_user=True, loglevel='error', action='ignore')
+        pane.errorPane(str(exception))
+
     def onLoadingRelatedMethod(self,table,sqlContextName=None):
         return 'onLoading_%s' % table.replace('.', '_')
     
@@ -101,34 +109,46 @@ class TableHandlerCommon(BaseComponent):
         rootCode = rootCode or table.replace('.','_')
         self._th_mixinResource(rootCode=rootCode,table=table,resourceName=resourceName)
 
-    def _th_mixinResource(self,rootCode=None,table=None,resourceName=None,defaultClass=None):
+    def _th_mixinResource(self,rootCode=None,table=None,resourceName=None,defaultClass=None,pane=None):
         pkg,tablename = table.split('.')
         defaultModule = 'th_%s' %tablename
         resourcePath = self._th_getResourceName(resourceName,defaultModule,defaultClass)
         tableObj = self.db.table(table)
         pluginId = getattr(tableObj, '_pluginId', None)
-        self.mixinComponent(resourcePath,mangling_th=rootCode,safeMode=True)
-        self.mixinComponent('tables',tablename,resourcePath,pkg=pkg,mangling_th=rootCode, pluginId=pluginId, pkgOnly=True,safeMode=True)
+        found = self.mixinComponent(resourcePath,mangling_th=rootCode,safeMode=True)
+        found = self.mixinComponent('tables',tablename,resourcePath,pkg=pkg,mangling_th=rootCode, pluginId=pluginId, pkgOnly=True,safeMode=True) or found
         project_mainpackage = self.package.attributes.get('mainpkg')
         if project_mainpackage:
-            self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=project_mainpackage,mangling_th=rootCode, pkgOnly=True,safeMode=True)
-        
+            found = self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=project_mainpackage,mangling_th=rootCode, pkgOnly=True,safeMode=True) or found
+
         enabled_packages = self._call_kwargs.get('enabled_packages','*')
         if enabled_packages=='*':
-            enabled_packages = list(self.application.packages.keys()) 
+            enabled_packages = list(self.application.packages.keys())
         else:
             enabled_packages = enabled_packages.split(',')
         for refpkg in enabled_packages:
             if refpkg!=self.package.name:
-                self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=refpkg,mangling_th=rootCode, pkgOnly=True,safeMode=True)
-        self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=self.package.name,mangling_th=rootCode, pkgOnly=True,safeMode=True)
+                found = self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=refpkg,mangling_th=rootCode, pkgOnly=True,safeMode=True) or found
+        found = self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=self.package.name,mangling_th=rootCode, pkgOnly=True,safeMode=True) or found
+        if not found:
+            exc = GnrMixinNotFound("Missing resource '%s' for table '%s'" % (defaultClass or '?', table))
+            if pane is not None:
+                self._th_missingResource(pane, exc)
+                return None
+            raise exc
         return resourcePath
     
-    def _th_getResClass(self,table=None,resourceName=None,defaultClass=None):
+    def _th_getResClass(self,table=None,resourceName=None,defaultClass=None,pane=None):
         pkg,tablename = table.split('.')
         defaultModule = 'th_%s' %tablename
         resourceName = self._th_getResourceName(resourceName,defaultModule,defaultClass)
-        return self.importTableResource(table,resourceName)
+        try:
+            return self.importTableResource(table,resourceName)
+        except GnrMissingResourceException as e:
+            if pane is not None:
+                self._th_missingResource(pane, e)
+                return None
+            raise
             
     def _th_getOptions(self,mangler=None):
         if isinstance(mangler,Bag):

resources/common/th/th_view.py
@@ -33,7 +33,8 @@ class TableHandlerView(BaseComponent):
                        virtualStore=None,condition=None,condition_kwargs=None,
                        structure_field=None,structure_field_kwargs=None,sections_kwargs=None,
                        store_kwargs=None,extendedQuery=None,**kwargs):
-        self._th_mixinResource(frameCode,table=table,resourceName=viewResource,defaultClass='View')
+        if not self._th_mixinResource(frameCode,table=table,resourceName=viewResource,defaultClass='View',pane=pane):
+            return
         options = self._th_getOptions(frameCode)
         if extendedQuery is None:
             extendedQuery = options.get('extendedQuery')

gnrjs/gnr_d11/css/gnrbase_css/01_gnr_variables.css
@@ -264,7 +264,51 @@
     --status-warning: var(--color-warning); /* @theme */
     --status-idle: var(--gray-300); /* @theme */
 
-    /* --- Toaster --- */
+    /* --- Toast (gnr-toast notification cards) --- */
+    --toast-container-gap: 8px;
+    --toast-container-top: 16px;
+    --toast-container-right: 16px;
+    --toast-min-width: 300px;
+    --toast-max-width: 420px;
+    --toast-padding: 14px 16px;
+    --toast-gap: 10px;
+    --toast-border-width: 4px;
+    --toast-icon-size: 20px;
+    --toast-btn-size: 18px;
+    --toast-progress-height: 3px;
+
+    /* --- Traceback viewer (gnr-tb) --- */
+    --tb-font-size: 13px;
+    --tb-mono-font: "SF Mono", Menlo, monospace;
+    --tb-frame-border: var(--gray-200);
+    --tb-frame-border-open: var(--gray-300);
+    --tb-frame-border-error: #f87171;
+    --tb-frame-radius: 8px;
+    --tb-summary-bg: #fafbfc;
+    --tb-summary-hover: #f0f4f8;
+    --tb-num-bg: var(--gray-200);
+    --tb-num-color: #666;
+    --tb-num-error-bg: #fee2e2;
+    --tb-module-color: var(--accent-color);
+    --tb-func-color: #7c3aed;
+    --tb-muted-color: #999;
+    --tb-hash-color: #9ca3af;
+    --tb-hash-bg: var(--gray-100);
+    --tb-locals-badge-bg: #dbeafe;
+    --tb-locals-hdr-bg: #f8fafc;
+    --tb-locals-th-bg: #f1f5f9;
+    --tb-code-bg: #1e293b;
+    --tb-code-color: #e2e8f0;
+    --tb-error-bg: #fef2f2;
+    --tb-error-color: #dc2626;
+    --tb-error-border: #fecaca;
+    --tb-btn-bg: #f8f9fa;
+    --tb-btn-border: #ddd;
+    --tb-btn-color: #555;
+    --tb-btn-hover-bg: #e9ecef;
+    --tb-btn-hover-border: #ccc;
+
+    /* --- Toaster (legacy dojox.widget.Toaster) --- */
     --toaster-success-bg: #70BF73; /* @theme — color-success #4CAF50 80% + white */
     --toaster-error-bg: #8C2320;   /* @theme — color-error #E53935 60% + black */
     --toaster-warning-bg: #FFA726; /* @theme — color-warning #FF9800 85% + white */

commit ae12d145eeec31ec3e1a3f982b2d78bbbadc84da

fix: _th_mixinResource safeMode for tables without th_ resource, multidomain error URLs
Add safeMode parameter to _th_mixinResource so that tolerant callers
(th_tableViewer, th_tableEditor, th_thFormHandler) can proceed with
framework defaults when no table-specific mixin is found. This fixes
a regression from 2222f99db where lookup tables without explicit th_
files failed with GnrMixinNotFound.

Also fix error detail URLs in multidomain apps: current_domain is now
saved only when multidomain is active, and the detail_url formula uses
/_main_/ prefix since errors are centralized in the primary database.

gnrpy/gnr/web/gnrwsgisite.py
@@ -1016,7 +1016,8 @@ class GnrWsgiSite(object):
                 kwargs['request_uri'] = request.url
                 kwargs['request_host'] = request.host_url
 
-        kwargs.setdefault('current_domain', self.currentDomain)
+        if self.multidomain:
+            kwargs.setdefault('current_domain', self.currentDomain)
         error_id = self.gnrapp.errorHandler(exception=exception, **kwargs)
         if error_id and page:
             notify_user = kwargs.get('notify_user')

projects/gnrcore/packages/sys/model/error.py
@@ -23,8 +23,13 @@ class Table(object):
         tbl.column('error_type',name_long='!!Error type')
         tbl.column('error_code',name_long='!!Error code',indexed=True)
         tbl.formulaColumn('detail_url',
-                          """(CASE WHEN $request_host IS NULL THEN '/sys/ep_error?error_id=' || $id
-                                ELSE $request_host || '/sys/ep_error?error_id=' || $id
+                          """(CASE WHEN $request_host IS NULL
+                                THEN CASE WHEN $current_domain IS NOT NULL
+                                    THEN '/_main_/sys/ep_error?error_id=' || $id
+                                    ELSE '/sys/ep_error?error_id=' || $id END
+                                ELSE $request_host || CASE WHEN $current_domain IS NOT NULL
+                                    THEN '/_main_/sys/ep_error?error_id=' || $id
+                                    ELSE '/sys/ep_error?error_id=' || $id END
                              END)
                             """,
                           name_long='!!Detail')

resources/common/th/th_form.py
@@ -20,7 +20,7 @@ class TableHandlerForm(BaseComponent):
     def th_tableEditor(self,pane,frameCode=None,table=None,th_pkey=None,formResource=None,
                         formInIframe=False,dfltoption_kwargs=None,**kwargs):
         table = table or pane.attributes.get('table')
-        resourcePath = self._th_mixinResource(frameCode,table=table,resourceName=formResource,defaultClass='Form',pane=pane)
+        resourcePath = self._th_mixinResource(frameCode,table=table,resourceName=formResource,defaultClass='Form',pane=pane,safeMode=True)
         if not resourcePath:
             return
         options = dfltoption_kwargs
@@ -133,7 +133,7 @@ class TableHandlerForm(BaseComponent):
                         store='recordCluster',handlerType=None,tree_kwargs=None,**kwargs):
         tableCode = table.replace('.','_')
         formId = formId or tableCode
-        resourcePath = self._th_mixinResource(formId,table=table,resourceName=formResource,defaultClass='Form',pane=pane)
+        resourcePath = self._th_mixinResource(formId,table=table,resourceName=formResource,defaultClass='Form',pane=pane,safeMode=True)
         if not resourcePath:
             return
         resource_options = self._th_getOptions(formId)

resources/common/th/th_lib.py
@@ -109,7 +109,7 @@ class TableHandlerCommon(BaseComponent):
         rootCode = rootCode or table.replace('.','_')
         self._th_mixinResource(rootCode=rootCode,table=table,resourceName=resourceName)
 
-    def _th_mixinResource(self,rootCode=None,table=None,resourceName=None,defaultClass=None,pane=None):
+    def _th_mixinResource(self,rootCode=None,table=None,resourceName=None,defaultClass=None,pane=None,safeMode=False):
         pkg,tablename = table.split('.')
         defaultModule = 'th_%s' %tablename
         resourcePath = self._th_getResourceName(resourceName,defaultModule,defaultClass)
@@ -131,6 +131,8 @@ class TableHandlerCommon(BaseComponent):
                 found = self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=refpkg,mangling_th=rootCode, pkgOnly=True,safeMode=True) or found
         found = self.mixinComponent('tables','_packages',pkg,tablename,resourcePath,pkg=self.package.name,mangling_th=rootCode, pkgOnly=True,safeMode=True) or found
         if not found:
+            if safeMode:
+                return resourcePath
             exc = GnrMixinNotFound("Missing resource '%s' for table '%s'" % (defaultClass or '?', table))
             if pane is not None:
                 self._th_missingResource(pane, exc)
@@ -149,7 +151,7 @@ class TableHandlerCommon(BaseComponent):
                 self._th_missingResource(pane, e)
                 return None
             raise
-            
+
     def _th_getOptions(self,mangler=None):
         if isinstance(mangler,Bag):
             inattr = mangler.getInheritedAttributes()

resources/common/th/th_view.py
@@ -33,7 +33,7 @@ class TableHandlerView(BaseComponent):
                        virtualStore=None,condition=None,condition_kwargs=None,
                        structure_field=None,structure_field_kwargs=None,sections_kwargs=None,
                        store_kwargs=None,extendedQuery=None,**kwargs):
-        if not self._th_mixinResource(frameCode,table=table,resourceName=viewResource,defaultClass='View',pane=pane):
+        if not self._th_mixinResource(frameCode,table=table,resourceName=viewResource,defaultClass='View',pane=pane,safeMode=True):
             return
         options = self._th_getOptions(frameCode)
         if extendedQuery is None: