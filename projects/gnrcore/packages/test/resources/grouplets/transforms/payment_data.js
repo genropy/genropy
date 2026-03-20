dd2aeb772e6844d441f28a0cd0dda1f2fea590a7
fporcari <francesco.porcari@softwell.it>
2026-03-20 12:40:02 +0100

    refactor: rename grouplet test files and translate Italian labels to English
    Rename dati_generali -> general_data, dati_pagamento -> payment_data.
    Translate all captions, labels, data keys and docstrings to English
    for consistency across the test suite.

--- projects/gnrcore/packages/test/resources/grouplets/transforms/payment_data.js
+++ projects/gnrcore/packages/test/resources/grouplets/transforms/payment_data.js
@@ -1,11 +1,11 @@
-const dati_pagamento = {
+const payment_data = {
     onLoading: function(data) {
         const nodes = data.getNodes();
         const gridBag = new gnr.GnrBag();
         let i = 0;
         const toRemove = [];
         for (const [k, node] of nodes.entries()) {
-            if (node.label === 'DettaglioPagamento') {
+            if (node.label === 'PaymentDetail') {
                 const val = node.getValue();
                 if (val instanceof gnr.GnrBag) {
                     gridBag.setItem('r_' + i, val.deepCopy());
@@ -20,28 +20,28 @@ const dati_pagamento = {
             data.delItem('#' + idx);
         }
         if (i > 0) {
-            data.setItem('DettaglioPagamento', gridBag);
+            data.setItem('PaymentDetail', gridBag);
         }
     },
 
     onSaving: function(data, sourceBag) {
-        const dettaglio = data.getItem('DettaglioPagamento');
+        const dettaglio = data.getItem('PaymentDetail');
         if (!dettaglio) {
             return;
         }
-        data.delItem('DettaglioPagamento');
+        data.delItem('PaymentDetail');
         const nodes = sourceBag.getNodes();
         for (let k = nodes.length - 1; k >= 0; k--) {
-            if (nodes[k].label === 'DettaglioPagamento') {
+            if (nodes[k].label === 'PaymentDetail') {
                 sourceBag.delItem('#' + k);
             }
         }
         for (const n of dettaglio.getNodes()) {
             const val = n.getValue();
             if (val instanceof gnr.GnrBag) {
-                sourceBag.addItem('DettaglioPagamento', val.deepCopy());
+                sourceBag.addItem('PaymentDetail', val.deepCopy());
             } else {
-                sourceBag.addItem('DettaglioPagamento', new gnr.GnrBag(val));
+                sourceBag.addItem('PaymentDetail', new gnr.GnrBag(val));
             }
         }
     }
--- projects/gnrcore/packages/test/webpages/gnrwdg/test_grouplets/10_grouplet_transform.py
+++ projects/gnrcore/packages/test/webpages/gnrwdg/test_grouplets/10_grouplet_transform.py
@@ -3,8 +3,8 @@
 """Test page for onLoading/onSaving grouplet hooks: end-to-end validation
 of data transformation during load and save in GroupletForm with Item store.
 
-The DatiPagamento grouplet defines:
-- onLoading: wraps DettaglioPagamento children (0, 1, ...) into BagGrid format (r_0, r_1, ...)
+The PaymentData grouplet defines:
+- onLoading: wraps PaymentDetail children (0, 1, ...) into BagGrid format (r_0, r_1, ...)
 - onSaving: unwraps BagGrid format back to flat numbered children
 
 Test flow: load initial data -> verify grid display -> edit -> save -> check raw data"""
@@ -17,47 +17,46 @@ class GnrCustomWebPage(object):
                      gnrcomponents/formhandler:FormHandler,
                      gnrcomponents/grouplet/grouplet:GroupletHandler"""
 
-    def _sample_dati_generali(self):
-        """FatturaPA-like DatiGeneraliDocumento structure"""
+    def _sample_general_data(self):
+        """Sample general document data structure"""
         data = Bag()
-        data['TipoDocumento'] = 'TD01'
-        data['Divisa'] = 'EUR'
-        data['Data'] = '2024-01-15::D'
-        data['Numero'] = '2024/001'
-        data['ImportoTotaleDocumento'] = 1750.00
+        data['DocumentType'] = 'TD01'
+        data['Currency'] = 'EUR'
+        data['Date'] = '2024-01-15::D'
+        data['Number'] = '2024/001'
+        data['TotalDocumentAmount'] = 1750.00
         return data
 
-    def _sample_dati_pagamento(self):
-        """FatturaPA-like DatiPagamento structure with duplicate DettaglioPagamento labels"""
+    def _sample_payment_data(self):
+        """Sample payment data structure with duplicate PaymentDetail labels"""
         data = Bag()
-        data['CondizioniPagamento'] = 'TP02'
-        data.addItem('DettaglioPagamento', Bag(dict(
-            ModalitaPagamento='MP05',
-            DataScadenzaPagamento='2024-01-30::D',
-            ImportoPagamento=1000.00
+        data['PaymentTerms'] = 'TP02'
+        data.addItem('PaymentDetail', Bag(dict(
+            PaymentMethod='MP05',
+            PaymentDueDate='2024-01-30::D',
+            PaymentAmount=1000.00
         )))
-        data.addItem('DettaglioPagamento', Bag(dict(
-            ModalitaPagamento='MP01',
-            DataScadenzaPagamento='2024-02-28::D',
-            ImportoPagamento=500.00
+        data.addItem('PaymentDetail', Bag(dict(
+            PaymentMethod='MP01',
+            PaymentDueDate='2024-02-28::D',
+            PaymentAmount=500.00
         )))
-        data.addItem('DettaglioPagamento', Bag(dict(
-            ModalitaPagamento='MP08',
-            DataScadenzaPagamento='2024-03-31::D',
-            ImportoPagamento=250.00
+        data.addItem('PaymentDetail', Bag(dict(
+            PaymentMethod='MP08',
+            PaymentDueDate='2024-03-31::D',
+            PaymentAmount=250.00
         )))
         return data
 
     def test_1_transform_panel(self, pane):
-        """GroupletPanel with transforms grouplets: select DatiPagamento to see
+        """GroupletPanel with transforms grouplets: select PaymentData to see
         onLoading transform flat data into BagGrid rows, edit, then verify
         onSaving restores flat structure"""
-        pane.data('.data.dati_generali', self._sample_dati_generali())
-        pane.data('.data.dati_pagamento', self._sample_dati_pagamento())
+        pane.data('.data.general_data', self._sample_general_data())
+        pane.data('.data.payment_data', self._sample_payment_data())
         bc = pane.borderContainer(height='500px', border='1px solid silver')
         bc.groupletPanel(
             topic='transforms',
             value='^.data',
             frameCode='transform_panel',
             region='center')
-
