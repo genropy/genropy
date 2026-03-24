4a97275c98799b949991faf4c046ea91efedfc52
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2026-03-24 15:16:15 +0100

    have gunicorn started as serveprod (via gnrstack, default on k8s) to read the gunicorn configuration file provided by the original docker image, which can be customized.

--- gnrpy/gnr/web/cli/gnrserveprod.py
+++ gnrpy/gnr/web/cli/gnrserveprod.py
@@ -2,7 +2,8 @@
 # encoding: utf-8
 
 import os
-    
+import os.path
+
 from gunicorn.app.base import BaseApplication
 from gunicorn.config import Config as GunicornConfig
 
@@ -124,9 +125,20 @@ def main():
     app = get_gnr_wsgi_application(cli_config.get("instance_name"), cli_config)
 
     file_config = {}
-    if "config" in cli_config:
-        file_config = load_config_file(cli_config["config"])
     
+    if os.environ.get("KUBERNETES_SERVICE_HOST", None):
+        default_docker_gunicorn_conf = "/home/genro/gunicorn.py"
+        if os.path.exists(default_docker_gunicorn_conf):
+            file_config.update(load_config_file(default_docker_gunicorn_conf))
+
+    if "config" in cli_config:
+        if os.path.exists(cli_config["config"]):
+            file_config.update(load_config_file(cli_config["config"]))
+
+    if custom_conf_file := os.environ.get("GNR_CUSTOM_GUNICORN_CONF", None):
+        if os.path.exists(custom_conf_file):
+            file_config.update(load_config_file(custom_conf_file))
+
     # precedence order: command line options, environment, configuration file
     combined_config = {**file_config, **env_config, **cli_config}        
     GnrProductionServer(app, combined_config).run()
