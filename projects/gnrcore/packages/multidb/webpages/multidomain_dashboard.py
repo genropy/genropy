ab0cbcabcd0bdf5a3c570255c5a738c7cdc6f95f
fporcari <francesco.porcari@softwell.it>
2026-05-20 16:05:23 +0200

    fix multidomain_dashboard internal paths

--- projects/gnrcore/packages/multidb/webpages/multidomain_dashboard.py
+++ projects/gnrcore/packages/multidb/webpages/multidomain_dashboard.py
@@ -33,9 +33,9 @@ class GnrCustomWebPage(object):
         frame.top.slotToolbar('5,stackButtons,*', height='28px')
         sc = frame.center.stackContainer(margin='2px')
         self._buildTablesTab(sc.borderContainer(title='!!Tables',
-                                                datapath='.tables'))
+                                                datapath='.tables_alignment'))
         self._buildUserObjectsTab(sc.borderContainer(title='!!User Objects',
-                                                     datapath='.userobjects'))
+                                                     datapath='.userobjects_alignment'))
 
     def _injectStyles(self, frame):
         frame.css("._common_d11 .toprop .dojoxGrid-cell",
@@ -52,68 +52,74 @@ class GnrCustomWebPage(object):
     # ------------------------------------------------------------------
 
     def _buildTablesTab(self, bc):
+        # bc datapath = main.tables_alignment
         # Toolbar: pick secondary store + table
         bar = bc.contentPane(region='top').slotToolbar('5,sel,*', height='34px')
         fb = bar.sel.formbuilder(cols=4, border_spacing='3px')
-        fb.filteringSelect(value='^.store_secondary', lbl='Secondary',
+        fb.filteringSelect(value='^main.tables_alignment.store_secondary',
+                           lbl='Secondary',
                            width='12em', values=self._storeOptions())
-        fb.remoteSelect(value='^.sync_table', lbl='Table', width='22em',
+        fb.remoteSelect(value='^main.tables_alignment.sync_table',
+                        lbl='Table', width='22em',
                         method=self.getPropagableTables, hasDownArrow=True)
         bc.dataRpc(
-            '.sync_done', self.setSyncInfo,
-            insync_table='^.sync_table',
-            instore_secondary='^.store_secondary',
-            instate='tables',
+            'main.tables_alignment.sync_done', self.setSyncInfo,
+            insync_table='^main.tables_alignment.sync_table',
+            instore_secondary='^main.tables_alignment.store_secondary',
+            instate='tables_alignment',
             _if='insync_table && instore_secondary',
-            _onResult='FIRE .build_th; FIRE .load_th;')
+            _onResult='FIRE main.tables_alignment.build_th; '
+                      'FIRE main.tables_alignment.load_th;')
 
         body = bc.borderContainer(region='center')
 
-        # Primary pane (rootstore)
+        # Primary pane (rootstore) — datapath = main.tables_alignment.th_primary
         primary = body.framePane(margin='3px', border='1px solid silver',
-                                 rounded=6, region='left', width='50%')
+                                 rounded=6, region='left', width='50%',
+                                 datapath='.th_primary')
         primary.top.slotToolbar('5,store_lbl,*', height='23px') \
             .store_lbl.div('Primary (rootstore)',
                            font_weight='bold', color='#666')
         primary.center.contentPane(margin='5px', overflow='hidden') \
             .dynamicTableHandler(
-                table='=main.tables.sync_table',
-                datapath='main.dbroot_primary',
+                table='=main.tables_alignment.sync_table',
+                datapath='.view',
                 th_wdg='plain',
                 th_viewResource='View',
                 th_configurable=True,
                 th_view_store_applymethod='checksync_primary',
-                th_view_store_state='tables',
+                th_view_store_state='tables_alignment',
                 th_view_store_onStart=True,
                 th_grid_onDrag="dragValues['tbl_from_primary']=dragValues.gridrow.rowset;",
                 th_grid_dropTarget_grid='tbl_from_secondary',
                 th_grid_onDrop_tbl_from_secondary='genro.publish("tbl_drop_primary",{data:data});',
-                th_grid_selectedId='main.tables.selected_pkey_primary',
-                th_view_store__fired='^main.tables.load_th',
-                nodeId='tables_primary_th',
-                _fired='^main.tables.build_th')
+                th_grid_selectedId='main.tables_alignment.th_primary.selected_pkey',
+                th_view_store__fired='^main.tables_alignment.load_th',
+                nodeId='th_tables_primary',
+                _fired='^main.tables_alignment.build_th')
         primary.bottom.slotToolbar('*,prop_btn,5', height='28px') \
             .prop_btn.button('!!Propagate to all stores',
                              publish='tables_propagate_all',
-                             disabled='^main.tables.selected_pkey_primary?=!#v')
+                             disabled='^main.tables_alignment.th_primary.selected_pkey?=!#v')
 
-        # Secondary pane (chosen secondary store)
-        sec_store = '=main.tables.store_secondary'
+        # Secondary pane — datapath = main.tables_alignment.th_secondary
+        sec_store = '=main.tables_alignment.store_secondary'
         secondary = body.framePane(margin='3px', border='1px solid silver',
-                                   rounded=6, region='center')
+                                   rounded=6, region='center',
+                                   datapath='.th_secondary')
         secondary.top.slotToolbar('5,store_lbl,*', height='23px') \
             .store_lbl.div("==s||'(no secondary store)'",
-                           s='^main.tables.store_secondary',
+                           s='^main.tables_alignment.store_secondary',
                            font_weight='bold', color='#666')
         secondary.center.contentPane(margin='5px', overflow='hidden') \
             .dynamicTableHandler(
-                table='=main.tables.sync_table',
-                datapath='main.dbroot_secondary',
+                table='=main.tables_alignment.sync_table',
+                datapath='.view',
                 th_wdg='plain',
                 th_viewResource='View',
                 th_configurable=True,
                 th_view_store_applymethod='checksync_secondary',
-                th_view_store_state='tables',
+                th_view_store_state='tables_alignment',
                 th_view_store_currentDbstore=sec_store,
                 th_view_store_forced_dbstore=True,
                 th_dbstore=sec_store,
@@ -121,18 +127,18 @@ class GnrCustomWebPage(object):
                 th_grid_onDrag="dragValues['tbl_from_secondary']=dragValues.gridrow.rowset;",
                 th_grid_dropTarget_grid='tbl_from_primary',
                 th_grid_onDrop_tbl_from_primary='genro.publish("tbl_drop_secondary",{data:data});',
-                th_view_store__fired='^main.tables.load_th',
-                nodeId='tables_secondary_th',
-                _fired='^main.tables.build_th')
+                th_view_store__fired='^main.tables_alignment.load_th',
+                nodeId='th_tables_secondary',
+                _fired='^main.tables_alignment.build_th')
 
         # Propagate-to-all RPC (button is in primary.bottom toolbar)
         body.dataRpc(
             'dummy', self.propagateToAllStores,
             subscribe_tables_propagate_all=True,
-            table='=main.tables.sync_table',
-            pkey='=main.tables.selected_pkey_primary',
+            table='=main.tables_alignment.sync_table',
+            pkey='=main.tables_alignment.th_primary.selected_pkey',
             _if='table && pkey',
-            _onResult='FIRE main.tables.load_th; '
+            _onResult='FIRE main.tables_alignment.load_th; '
                       'genro.dlg.alert("Done","Propagated to all stores");',
             _lockScreen=True, timeout=0,
             _ask=dict(title='!!Propagate to all stores',
@@ -150,18 +156,18 @@ class GnrCustomWebPage(object):
         body.dataRpc(
             'dummy', self.copyRows,
             subscribe_tbl_drop_primary=True,
-            store_from='=main.tables.store_secondary', store_to=False,
-            table='=main.tables.sync_table',
+            store_from='=main.tables_alignment.store_secondary', store_to=False,
+            table='=main.tables_alignment.sync_table',
             _onCalling=oncalling,
-            _onResult='FIRE main.tables.load_th;',
+            _onResult='FIRE main.tables_alignment.load_th;',
             _lockScreen=True, timeout=0)
         body.dataRpc(
             'dummy', self.copyRows,
             subscribe_tbl_drop_secondary=True,
-            store_from=False, store_to='=main.tables.store_secondary',
-            table='=main.tables.sync_table',
+            store_from=False, store_to='=main.tables_alignment.store_secondary',
+            table='=main.tables_alignment.sync_table',
             _onCalling=oncalling,
-            _onResult='FIRE main.tables.load_th;',
+            _onResult='FIRE main.tables_alignment.load_th;',
             _lockScreen=True, timeout=0)
 
     # ------------------------------------------------------------------
@@ -169,95 +175,106 @@ class GnrCustomWebPage(object):
     # ------------------------------------------------------------------
 
     def _buildUserObjectsTab(self, bc):
+        # bc datapath = main.userobjects_alignment
         # Toolbar: pick secondary store + table filter + objtype filter
         bar = bc.contentPane(region='top').slotToolbar('5,sel,*', height='34px')
         fb = bar.sel.formbuilder(cols=4, border_spacing='3px')
-        fb.filteringSelect(value='^.store_secondary', lbl='Secondary',
+        fb.filteringSelect(value='^main.userobjects_alignment.store_secondary',
+                           lbl='Secondary',
                            width='12em', values=self._storeOptions())
-        fb.remoteSelect(value='^.uo_tbl', lbl='Table', width='18em',
+        fb.remoteSelect(value='^main.userobjects_alignment.uo_tbl',
+                        lbl='Table', width='18em',
                         method=self.getUserObjectTables, hasDownArrow=True)
-        fb.remoteSelect(value='^.uo_objtype', lbl='Type', width='12em',
+        fb.remoteSelect(value='^main.userobjects_alignment.uo_objtype',
+                        lbl='Type', width='12em',
                         method=self.getUserObjectTypes,
-                        condition_uo_tbl='=.uo_tbl',
+                        condition_uo_tbl='=main.userobjects_alignment.uo_tbl',
                         hasDownArrow=True)
-        bc.dataController('SET .uo_objtype = null;', _trigger='^.uo_tbl')
+        bc.dataController(
+            'SET main.userobjects_alignment.uo_objtype = null;',
+            _trigger='^main.userobjects_alignment.uo_tbl')
         bc.dataRpc(
-            '.sync_done', self.setSyncInfo,
+            'main.userobjects_alignment.sync_done', self.setSyncInfo,
             insync_table=self.USEROBJECT_TABLE,
-            instore_secondary='^.store_secondary',
+            instore_secondary='^main.userobjects_alignment.store_secondary',
             where='$tbl=:c_tbl AND ($objtype=:c_objtype OR :c_objtype IS NULL)',
-            c_tbl='^.uo_tbl',
-            c_objtype='^.uo_objtype',
-            instate='userobjects',
+            c_tbl='^main.userobjects_alignment.uo_tbl',
+            c_objtype='^main.userobjects_alignment.uo_objtype',
+            instate='userobjects_alignment',
             _if='instore_secondary && c_tbl',
-            _onResult='FIRE .load_th;')
+            _onResult='FIRE main.userobjects_alignment.load_th;')
 
         body = bc.borderContainer(region='center')
         condition = '$tbl=:c_tbl AND ($objtype=:c_objtype OR :c_objtype IS NULL)'
 
-        # Primary pane (rootstore)
+        # Primary pane (rootstore) — datapath = main.userobjects_alignment.th_primary
         primary = body.framePane(margin='3px', border='1px solid silver',
-                                 rounded=6, region='left', width='50%')
+                                 rounded=6, region='left', width='50%',
+                                 datapath='.th_primary')
         primary.top.slotToolbar('5,store_lbl,*', height='23px') \
             .store_lbl.div('Primary (rootstore)',
                            font_weight='bold', color='#666')
         primary.center.contentPane(margin='5px', overflow='hidden') \
             .plainTableHandler(
                 table=self.USEROBJECT_TABLE,
+                datapath='.view',
                 viewResource='ViewMultidomain',
                 configurable=True,
                 condition=condition,
-                condition_c_tbl='^main.userobjects.uo_tbl',
-                condition_c_objtype='^main.userobjects.uo_objtype',
+                condition_c_tbl='^main.userobjects_alignment.uo_tbl',
+                condition_c_objtype='^main.userobjects_alignment.uo_objtype',
                 view_store_applymethod='checksync_primary',
-                view_store_state='userobjects',
+                view_store_state='userobjects_alignment',
                 view_store_onStart=True,
-                view_store__fired='^main.userobjects.load_th',
+                view_store__fired='^main.userobjects_alignment.load_th',
                 grid_onDrag="dragValues['uo_from_primary']=dragValues.gridrow.rowset;",
                 grid_dropTarget_grid='uo_from_secondary',
                 grid_onDrop_uo_from_secondary='genro.publish("uo_drop_primary",{data:data});',
-                nodeId='userobjects_primary_th')
+                grid_selectedId='main.userobjects_alignment.th_primary.selected_pkey',
+                nodeId='th_userobjects_primary')
         primary.bottom.slotToolbar('*,prop_btn,5', height='28px') \
             .prop_btn.button('!!Propagate to all stores',
                              publish='userobjects_propagate_all',
-                             disabled='^#userobjects_primary_th.view.grid.selectedId?=!#v')
+                             disabled='^main.userobjects_alignment.th_primary.selected_pkey?=!#v')
 
-        # Secondary pane (chosen secondary store)
-        sec_store = '=main.userobjects.store_secondary'
+        # Secondary pane — datapath = main.userobjects_alignment.th_secondary
+        sec_store = '=main.userobjects_alignment.store_secondary'
         secondary = body.framePane(margin='3px', border='1px solid silver',
-                                   rounded=6, region='center')
+                                   rounded=6, region='center',
+                                   datapath='.th_secondary')
         secondary.top.slotToolbar('5,store_lbl,*', height='23px') \
             .store_lbl.div("==s||'(no secondary store)'",
-                           s='^main.userobjects.store_secondary',
+                           s='^main.userobjects_alignment.store_secondary',
                            font_weight='bold', color='#666')
         secondary.center.contentPane(margin='5px', overflow='hidden') \
             .plainTableHandler(
                 table=self.USEROBJECT_TABLE,
+                datapath='.view',
                 viewResource='ViewMultidomain',
                 configurable=True,
                 condition=condition,
-                condition_c_tbl='^main.userobjects.uo_tbl',
-                condition_c_objtype='^main.userobjects.uo_objtype',
+                condition_c_tbl='^main.userobjects_alignment.uo_tbl',
+                condition_c_objtype='^main.userobjects_alignment.uo_objtype',
                 view_store_applymethod='checksync_secondary',
-                view_store_state='userobjects',
+                view_store_state='userobjects_alignment',
                 view_store_currentDbstore=sec_store,
                 view_store_forced_dbstore=True,
                 dbstore=sec_store,
                 view_store_onStart=True,
-                view_store__fired='^main.userobjects.load_th',
+                view_store__fired='^main.userobjects_alignment.load_th',
                 grid_onDrag="dragValues['uo_from_secondary']=dragValues.gridrow.rowset;",
                 grid_dropTarget_grid='uo_from_primary',
                 grid_onDrop_uo_from_primary='genro.publish("uo_drop_secondary",{data:data});',
-                nodeId='userobjects_secondary_th')
+                nodeId='th_userobjects_secondary')
 
         # Propagate-to-all RPC (button is in primary.bottom toolbar)
         body.dataRpc(
             'dummy', self.propagateToAllStores,
             subscribe_userobjects_propagate_all=True,
             table=self.USEROBJECT_TABLE,
-            pkey='=#userobjects_primary_th.view.grid.selectedId',
+            pkey='=main.userobjects_alignment.th_primary.selected_pkey',
             _if='pkey',
-            _onResult='FIRE main.userobjects.load_th; '
+            _onResult='FIRE main.userobjects_alignment.load_th; '
                       'genro.dlg.alert("Done","Propagated to all stores");',
             _lockScreen=True, timeout=0,
             _ask=dict(title='!!Propagate to all stores',
@@ -275,18 +292,18 @@ class GnrCustomWebPage(object):
         body.dataRpc(
             'dummy', self.copyRows,
             subscribe_uo_drop_primary=True,
-            store_from='=main.userobjects.store_secondary', store_to=False,
+            store_from='=main.userobjects_alignment.store_secondary', store_to=False,
             table=self.USEROBJECT_TABLE,
             _onCalling=oncalling,
-            _onResult='FIRE main.userobjects.load_th;',
+            _onResult='FIRE main.userobjects_alignment.load_th;',
             _lockScreen=True, timeout=0)
         body.dataRpc(
             'dummy', self.copyRows,
             subscribe_uo_drop_secondary=True,
-            store_from=False, store_to='=main.userobjects.store_secondary',
+            store_from=False, store_to='=main.userobjects_alignment.store_secondary',
             table=self.USEROBJECT_TABLE,
             _onCalling=oncalling,
-            _onResult='FIRE main.userobjects.load_th;',
+            _onResult='FIRE main.userobjects_alignment.load_th;',
             _lockScreen=True, timeout=0)
 
     # ------------------------------------------------------------------
